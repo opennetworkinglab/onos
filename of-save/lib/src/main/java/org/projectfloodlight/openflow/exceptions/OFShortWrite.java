7a9bcbe9c47358bb205ee5e2ab4392acad5dc896
tom <tom@onlab.us>
2014-08-28 09:11:16 -0700

    Removed of-save stuff.

--- of-save/lib/src/main/java/org/projectfloodlight/openflow/exceptions/OFShortWrite.java
+++
@@ -1,7 +0,0 @@
-package org.projectfloodlight.openflow.exceptions;
-
-public class OFShortWrite extends Exception {
-
-    private static final long serialVersionUID = 1L;
-
-}
--- of-save/lib/src/main/java/org/projectfloodlight/openflow/protocol/OFMessageReader.java
+++
@@ -1,8 +0,0 @@
-package org.projectfloodlight.openflow.protocol;
-
-import org.jboss.netty.buffer.ChannelBuffer;
-import org.projectfloodlight.openflow.exceptions.OFParseError;
-
-public interface OFMessageReader<T> {
-    T readFrom(ChannelBuffer bb) throws OFParseError;
-}
