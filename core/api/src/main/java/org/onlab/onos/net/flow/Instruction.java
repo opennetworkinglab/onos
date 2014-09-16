89e43ef4ea66981c9830e51640ea14134d80fcd7
alshabib <alshabibi.ali@gmail.com>
2014-09-16 10:36:34 -0700

    link discovery clean up

--- core/api/src/main/java/org/onlab/onos/net/flow/Instruction.java
+++ core/api/src/main/java/org/onlab/onos/net/flow/Instruction.java
@@ -35,8 +35,7 @@ public interface Instruction {
     // to create specific instructions.
 
     /**
-     * Returns the type of instruction not to be confused
-     * with the instruction's java type.
+     * Returns the type of instruction.
      * @return type of instruction
      */
     public Type type();
