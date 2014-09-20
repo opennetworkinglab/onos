5ca3437382d9064ce4f695d6ab591cecd6d66349
tom <tom@onlab.us>
2014-09-19 17:43:41 -0700

    Added a few javadocs. Split onos-install into install and config.

---
+++ core/store/src/main/java/org/onlab/onos/store/device/impl/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Implementation of device store using Hazelcast distributed structures.
+ */
+package org.onlab.onos.store.device.impl;
---
+++ core/store/src/main/java/org/onlab/onos/store/impl/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Common facilities for use by various distributed stores.
+ */
+package org.onlab.onos.store.impl;
