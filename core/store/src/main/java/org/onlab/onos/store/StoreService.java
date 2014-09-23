dc66b38ee43493b48ab9305e6c4edd97a5910e3e
tom <tom@onlab.us>
2014-09-22 17:05:47 -0700

    Fixing remote install; now starting as upstart daemon.

---
+++ core/store/src/main/java/org/onlab/onos/store/StoreService.java
@@ -0,0 +1,18 @@
+package org.onlab.onos.store;
+
+import com.hazelcast.core.HazelcastInstance;
+
+/**
+ * Bootstrap service to get a handle on a share Hazelcast instance.
+ */
+public interface StoreService {
+
+    /**
+     * Returns the shared Hazelcast instance for use as a distributed store
+     * backing.
+     *
+     * @return shared Hazelcast instance
+     */
+    HazelcastInstance getHazelcastInstance();
+
+}
---
+++ core/store/src/main/java/org/onlab/onos/store/impl/StoreManager.java
@@ -0,0 +1,39 @@
+package org.onlab.onos.store.impl;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import org.apache.felix.scr.annotations.Activate;
+import org.apache.felix.scr.annotations.Component;
+import org.apache.felix.scr.annotations.Deactivate;
+import org.apache.felix.scr.annotations.Service;
+import org.onlab.onos.store.StoreService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Auxiliary bootstrap of distributed store.
+ */
+@Component(immediate = true)
+@Service
+public class StoreManager implements StoreService {
+
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private HazelcastInstance instance;
+
+    @Activate
+    public void activate() {
+        instance = Hazelcast.newHazelcastInstance();
+        log.info("Started");
+    }
+
+    @Deactivate
+    public void deactivate() {
+        log.info("Stopped");
+    }
+
+    @Override
+    public HazelcastInstance getHazelcastInstance() {
+        return instance;
+    }
+}
