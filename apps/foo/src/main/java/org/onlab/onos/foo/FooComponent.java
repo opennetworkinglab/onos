0768a02c3983d9ed7195ff6ed2fc5484685410d5
tom <tom@onlab.us>
2014-09-24 16:16:16 -0700

    Added prox test cell and enhanced install to allow customization of the hazelcast.xml for proper network interface.

--- apps/foo/src/main/java/org/onlab/onos/foo/FooComponent.java
+++ apps/foo/src/main/java/org/onlab/onos/foo/FooComponent.java
@@ -8,6 +8,9 @@ import org.apache.felix.scr.annotations.ReferenceCardinality;
 import org.onlab.onos.cluster.ClusterEvent;
 import org.onlab.onos.cluster.ClusterEventListener;
 import org.onlab.onos.cluster.ClusterService;
+import org.onlab.onos.net.device.DeviceEvent;
+import org.onlab.onos.net.device.DeviceListener;
+import org.onlab.onos.net.device.DeviceService;
 import org.slf4j.Logger;
 
 import static org.slf4j.LoggerFactory.getLogger;
@@ -23,17 +26,23 @@ public class FooComponent {
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     protected ClusterService clusterService;
 
-    private ClusterEventListener clusterListener = new InnerClusterListener();
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected DeviceService deviceService;
+
+    private final ClusterEventListener clusterListener = new InnerClusterListener();
+    private final DeviceListener deviceListener = new InnerDeviceListener();
 
     @Activate
     public void activate() {
         clusterService.addListener(clusterListener);
+        deviceService.addListener(deviceListener);
         log.info("Started");
     }
 
     @Deactivate
     public void deactivate() {
         clusterService.removeListener(clusterListener);
+        deviceService.removeListener(deviceListener);
         log.info("Stopped");
     }
 
@@ -41,6 +50,13 @@ public class FooComponent {
         @Override
         public void event(ClusterEvent event) {
             log.info("WOOOOT! {}", event);
+        }
+    }
+
+    private class InnerDeviceListener implements DeviceListener {
+        @Override
+        public void event(DeviceEvent event) {
+            log.info("YEEEEHAAAAW! {}", event);
         }
     }
 }
