2482e6ff40b7db1a1df8cb71e09796e14531f447
tom <tom@onlab.us>
2014-10-01 16:51:48 -0700

    Enhanced cell & cells tools. Provided initial sketch of the Annotated concept for the model attributes.

--- core/api/src/main/java/org/onlab/onos/net/AbstractModel.java
+++ core/api/src/main/java/org/onlab/onos/net/AbstractModel.java
@@ -1,13 +1,21 @@
 package org.onlab.onos.net;
 
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Maps;
 import org.onlab.onos.net.provider.ProviderId;
+
+import java.util.Map;
+import java.util.Set;
 
 /**
  * Base implementation of a network model entity.
  */
-public class AbstractModel implements Provided {
+public class AbstractModel implements Provided, Annotated {
 
     private final ProviderId providerId;
+
+    // FIXME: figure out whether to make this concurrent or immutable
+    private final Map<String, String> annotations = Maps.newHashMap();
 
     // For serialization
     public AbstractModel() {
@@ -28,4 +36,13 @@ public class AbstractModel implements Provided {
         return providerId;
     }
 
+    @Override
+    public Set<String> annotationKeys() {
+        return ImmutableSet.copyOf(annotations.keySet());
+    }
+
+    @Override
+    public String annotation(String key) {
+        return annotations.get(key);
+    }
 }
