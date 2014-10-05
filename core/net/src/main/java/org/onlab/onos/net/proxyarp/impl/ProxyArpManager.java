3541bf29142762fcf32e7a370376ff39a6b2ebbf
Yuta HIGUCHI <y-higuchi@onlab.us>
2014-10-04 22:06:19 -0700

    bugfix was comparing DeviceId with Device
    Change-Id: I5b138b64c4e7b2e0e217218c225b59bc3e63337f

--- core/net/src/main/java/org/onlab/onos/net/proxyarp/impl/ProxyArpManager.java
+++ core/net/src/main/java/org/onlab/onos/net/proxyarp/impl/ProxyArpManager.java
@@ -206,12 +206,12 @@ public class ProxyArpManager implements ProxyArpService {
             for (Link l : links) {
                 // for each link, mark the concerned ports as internal
                 // and the remaining ports are therefore external.
-                if (l.src().deviceId().equals(d)
+                if (l.src().deviceId().equals(d.id())
                         && ports.contains(l.src().port())) {
                     ports.remove(l.src().port());
                     internalPorts.put(d, l.src().port());
                 }
-                if (l.dst().deviceId().equals(d)
+                if (l.dst().deviceId().equals(d.id())
                         && ports.contains(l.dst().port())) {
                     ports.remove(l.dst().port());
                     internalPorts.put(d, l.dst().port());
