8a85138a622bd86929ce1c3321a000b96546bba2
Yuta HIGUCHI <y-higuchi@onlab.us>
2014-10-06 14:56:46 -0700

    add hashCode, etc. to MessageSubject
    Change-Id: I5c0185ea6bc8ab37fc5bade1c64cc30be6a79f9a

--- core/store/dist/src/main/java/org/onlab/onos/store/cluster/messaging/MessageSubject.java
+++ core/store/dist/src/main/java/org/onlab/onos/store/cluster/messaging/MessageSubject.java
@@ -1,5 +1,9 @@
 package org.onlab.onos.store.cluster.messaging;
 
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.util.Objects;
+
 /**
  * Representation of a message subject.
  * Cluster messages have associated subjects that dictate how they get handled
@@ -10,7 +14,7 @@ public class MessageSubject {
     private final String value;
 
     public MessageSubject(String value) {
-        this.value = value;
+        this.value = checkNotNull(value);
     }
 
     public String value() {
@@ -20,5 +24,25 @@ public class MessageSubject {
     @Override
     public String toString() {
         return value;
+    }
+
+    @Override
+    public int hashCode() {
+        return value.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        MessageSubject that = (MessageSubject) obj;
+        return Objects.equals(this.value, that.value);
     }
 }
