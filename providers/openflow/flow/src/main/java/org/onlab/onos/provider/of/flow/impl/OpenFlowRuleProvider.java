a309989f49c7d96944eaaac865863d29999c4607
Yuta HIGUCHI <y-higuchi@onlab.us>
2014-10-17 12:27:14 -0700

    checkstyle fix
    Change-Id: I5912abd81b95b2422bfb75454b0fa1b7a9d63f5a

--- providers/openflow/flow/src/main/java/org/onlab/onos/provider/of/flow/impl/OpenFlowRuleProvider.java
+++ providers/openflow/flow/src/main/java/org/onlab/onos/provider/of/flow/impl/OpenFlowRuleProvider.java
@@ -62,7 +62,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.CancellationException;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutionException;
