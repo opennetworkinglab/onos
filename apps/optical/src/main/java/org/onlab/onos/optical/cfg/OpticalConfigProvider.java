2374fc9e454aa586f77f270a856484aa4beb1bd7
alshabib <alshabibi.ali@gmail.com>
2014-10-22 11:03:23 -0700

    many fixes from different people in various place. Optical, FlowRuleManager, LinkDiscovery
    Change-Id: Ie3302e80ee94bf2e41b3da7ac765557b1e7509fd

--- apps/optical/src/main/java/org/onlab/onos/optical/cfg/OpticalConfigProvider.java
+++ apps/optical/src/main/java/org/onlab/onos/optical/cfg/OpticalConfigProvider.java
@@ -83,7 +83,7 @@ public class OpticalConfigProvider extends AbstractProvider implements DevicePro
     protected OpticalNetworkConfig opticalNetworkConfig;
 
     public OpticalConfigProvider() {
-        super(new ProviderId("of", "org.onlab.onos.provider.opticalConfig", true));
+        super(new ProviderId("optical", "org.onlab.onos.provider.opticalConfig", true));
     }
 
     @Activate
@@ -238,7 +238,7 @@ public class OpticalConfigProvider extends AbstractProvider implements DevicePro
         while (iterWdmNode.hasNext()) {
             Roadm value = iterWdmNode.next();
             DeviceId did = deviceId("of:" + value.getNodeId().replace(":", ""));
-            ChassisId cid = new ChassisId(value.getNodeId());
+            ChassisId cid = new ChassisId();
             DefaultAnnotations extendedAttributes = DefaultAnnotations.builder()
                     .set(OPTICAL_ANNOTATION + "switchType", "ROADM")
                     .set(OPTICAL_ANNOTATION + "switchName", value.getName())
