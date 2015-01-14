8b35b9ead868d049d54ed7f2aafd1369e65a1b7a
unknown <j00273701@X00308683A4.china.huawei.com>
2015-01-14 16:12:56 +0800

    north bound app 3 and extend flowManager

--- apps/ipanagent/src/main/java/org/onosproject/ipran/IpranAgent.java
+++ apps/ipanagent/src/main/java/org/onosproject/ipran/IpranAgent.java
@@ -2,6 +2,12 @@ package org.onosproject.ipran;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
+import java.util.Collection;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
 import org.apache.felix.scr.annotations.Activate;
 import org.apache.felix.scr.annotations.Component;
 import org.apache.felix.scr.annotations.Deactivate;
@@ -16,14 +22,17 @@ import org.onosproject.cluster.LeadershipService;
 import org.onosproject.core.ApplicationId;
 import org.onosproject.core.CoreService;
 import org.onosproject.net.flow.FlowRuleService;
+import org.onosproject.net.flow.SncFlowRuleEntry;
 import org.onosproject.net.topology.TopologyEvent;
 import org.onosproject.net.topology.TopologyListener;
+import org.onosproject.net.topology.TopologyService;
 import org.onosproject.store.serializers.DecodeTo;
 import org.onosproject.store.serializers.StoreSerializer;
 import org.slf4j.Logger;
 
 import com.google.common.util.concurrent.Futures;
 import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 @Component(immediate = true)
 public class IpranAgent {
@@ -41,13 +50,26 @@ public class IpranAgent {
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     protected LeadershipService leadershipService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
+    protected TopologyService topologyService;
     
     private LeadershipEventListener leadershipEventListener =
             new InnerLeadershipEventListener();
+
+    private TopologyListener topologyListener =
+            new InnerTopologyEventListener();
+
+    private IpranSessionListener sessionListener =
+            new InnerSessionEventListener();
+
     private ApplicationId appId;
     private ControllerNode localControllerNode;
-    private IpranConnector ipranConnector;
+    private IpranSession ipranConnector;
+    // Stores all incoming route updates in a queue.
+    private BlockingQueue<Collection<SncFlowRuleEntry>> flowUpdatesQueue;
     private static final int DEFAULT_IPRAN_PORT = 2000;
+    private  ExecutorService flowUpdatesExecutor;
     
     @Activate
     protected void activate() {
@@ -56,18 +78,63 @@ public class IpranAgent {
         appId = coreService.registerApplication(IPRAN_APP);
 
         localControllerNode = clusterService.getLocalNode();
-        ipranConnector = new IpranConnector();
+        flowUpdatesQueue = new LinkedBlockingQueue<>();
+        ipranConnector = new IpranSession();
         ipranConnector.start();
+        topologyService.addListener(topologyListener);
         leadershipService.addListener(leadershipEventListener);
+        ipranConnector.addListener(sessionListener);
         leadershipService.runForLeadership(appId.name());
+        flowUpdatesExecutor = Executors.newSingleThreadExecutor(
+                                            new ThreadFactoryBuilder()
+                                            .setNameFormat("ipran-flow-updates-%d").build());
+        pocessFlowUpdate();
     }
 
+    private void pocessFlowUpdate() {
+        flowUpdatesExecutor.execute(new Runnable() {
+            @Override
+            public void run() {
+                doUpdatesThread();
+            }
+        });
+    }
+    private void doUpdatesThread() {
+        boolean interrupted = false;
+        try {
+            while (!interrupted) {
+                try {
+                    Collection<SncFlowRuleEntry> flowUpdates =
+                            flowUpdatesQueue.take();
+                    /* here should make some change:
+                     * first, use batch service interface
+                     * second, add asynchronous transaction
+                     */
+                    for (SncFlowRuleEntry flow : flowUpdates){
+                            flowService.applySncBatch(flow);
+                    }
+                } catch (InterruptedException e) {
+                    log.debug("Interrupted while taking from updates queue", e);
+                    interrupted = true;
+                } catch (Exception e) {
+                    log.debug("exception", e);
+                }
+            }
+        } finally {
+            if (interrupted) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
     @Deactivate
     protected void deactivate() {
         ipranConnector.stop();
         leadershipService.withdraw(appId.name());
         leadershipService.removeListener(leadershipEventListener);
-
+        topologyService.removeListener(topologyListener);
+        ipranConnector.removeListener(sessionListener);
+        flowUpdatesExecutor.shutdown();
+        flowUpdatesQueue.clear();
         log.info("IPRAN Stopped");
     }
   /**
@@ -82,8 +149,8 @@ public class IpranAgent {
                       event.time(), event.type(), event);
             Endpoint host = new Endpoint(localControllerNode.id().toString(), DEFAULT_IPRAN_PORT);
             ListenableFuture<byte[]> responseFuture = ipranConnector
-                    .sendAndRecvMsg(host, IpranConnector.messageType.TOPO_CHANGED.toString(), null);
-            Futures.transform(responseFuture, new DecodeTo<String>((StoreSerializer) IpranConnector.SERIALIZER));
+                    .sendAndRecvMsg(host, IpranSession.messageType.TOPO_CHANGED.toString(), null);
+            Futures.transform(responseFuture, new DecodeTo<String>((StoreSerializer) IpranSession.SERIALIZER));
             return;
         }
     }
@@ -116,6 +183,23 @@ public class IpranAgent {
                 break;
             default:
                 break;
+            }
+        }
+    }
+    /**
+     * A listener for flowRule downstream Events.
+     */
+    private class InnerSessionEventListener
+        implements IpranSessionListener {
+        @Override
+        public void update(Collection<SncFlowRuleEntry> flowUpdates) {
+            // TODO Auto-generated method stub
+            try {
+                flowUpdatesQueue.put(flowUpdates);
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                log.debug("Interrupted while putting on flowUpdatesQueue", e);
+                Thread.currentThread().interrupt();
             }
         }
     }
--- apps/ipanagent/src/main/java/org/onosproject/ipran/IpranSession.java
+++ apps/ipanagent/src/main/java/org/onosproject/ipran/IpranSession.java
@@ -3,6 +3,9 @@ import static org.onlab.util.Tools.namedThreads;
 import static org.slf4j.LoggerFactory.getLogger;
 
 import java.net.InetSocketAddress;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
@@ -18,6 +21,8 @@ import org.jboss.netty.channel.Channels;
 import org.jboss.netty.channel.socket.nio.NioClientSocketChannelFactory;
 import org.onlab.netty.Endpoint;
 import org.onosproject.ipran.serializers.KryoSerializer;
+import org.onosproject.net.flow.SncFlowRuleEntry;
+import org.onosproject.net.topology.TopologyListener;
 import org.slf4j.Logger;
 
 import com.google.common.cache.Cache;
@@ -27,13 +32,15 @@ import com.google.common.cache.RemovalNotification;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.SettableFuture;
 
-public class IpranConnector {
+public class IpranSession {
     private final Logger log = getLogger(getClass());
     boolean isShutdown = true;
     private ChannelFuture ipranChannel;     // connect to ipran connections
     private ClientBootstrap clientBootstrap;
     private final AtomicLong messageIdGenerator = new AtomicLong(0);
     private static final int DEFAULT_IPRAN_PORT = 2000;
+    InetSocketAddress listenAddress;
+    protected Set<IpranSessionListener> SessionListener = new HashSet<>();
     private final Cache<Long, SettableFuture<byte[]>> responseFutures = CacheBuilder.newBuilder()
             .maximumSize(100000)
             .expireAfterWrite(10, TimeUnit.SECONDS)
@@ -92,8 +99,7 @@ public class IpranConnector {
                     return pipeline;
                 }
             };
-        InetSocketAddress listenAddress =
-            new InetSocketAddress(DEFAULT_IPRAN_PORT);
+        listenAddress = new InetSocketAddress(DEFAULT_IPRAN_PORT);
 
         clientBootstrap = new ClientBootstrap(channelFactory);
         clientBootstrap.setPipelineFactory(pipelineFactory);
@@ -107,14 +113,30 @@ public class IpranConnector {
     }
 
     /**
-     * Stops the BGP Session Manager operation.
+     * Stops the Ipran Connection.
      */
     public void stop() {
         isShutdown = true;
         clientBootstrap.releaseExternalResources();
+        responseFutures.cleanUp();
     }
+
+    public void addListener(IpranSessionListener listener) {
+        SessionListener.add(listener);
+    }
+
+    public void removeListener(IpranSessionListener listener) {
+        SessionListener.remove(listener);
+    }
+
+    public void notify(Collection<SncFlowRuleEntry> flowUpdates) {
+        for (IpranSessionListener listener : SessionListener) {
+            listener.update(flowUpdates);
+        }
+    } 
+    
     /**
-     * send msg to ipran, make sure the connection has been built
+     * send msg to ipran, make sure the connection has been built.
      */   
     public ListenableFuture<byte[]>  sendAndRecvMsg(Endpoint host, String type, byte[] data) {
         SettableFuture<byte[]> futureResponse = SettableFuture.create();
@@ -129,7 +151,7 @@ public class IpranConnector {
         if(ipranChannel.getChannel() !=null) {
             ipranChannel.getChannel().write(message);
         } else {
-            this.start();
+            clientBootstrap.connect(listenAddress);
             ipranChannel.getChannel().write(message);
         }
         return futureResponse;
