e83175c0320f5ee4cdbcaceb8faf7abb091b623a
samueljcc <jiangchuncheng@huawei.com>
2015-01-19 15:34:45 +0800

    add test cases for extension core code

---
+++ core/api/src/test/java/org/onosproject/net/flowext/FlowRuleBatchExtRequestTest.java
@@ -0,0 +1,32 @@
+package org.onosproject.net.flowext;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import org.junit.Test;
+import org.onosproject.net.DeviceId;
+
+/** junit for FlowRuleBatchExtRequest constructor */
+public class FlowRuleBatchExtRequestTest {
+
+    @Test
+    public void testConstructor() {
+        String deviceId1 = "of:123456";
+        FlowRuleExtEntry entry1 = new FlowRuleExtEntry(DeviceId.deviceId(deviceId1), deviceId1.getBytes());
+        String deviceId2 = "of:234567";
+        FlowRuleExtEntry entry2 = new FlowRuleExtEntry(DeviceId.deviceId(deviceId2), deviceId2.getBytes());
+        Collection<FlowRuleExtEntry> toAdd = new ArrayList<FlowRuleExtEntry>();
+        toAdd.add(entry1);
+        toAdd.add(entry2);
+        FlowRuleBatchExtRequest request = new FlowRuleBatchExtRequest(1, toAdd);
+        assertThat(request.getBatch(), hasSize(2));
+        assertThat(request.batchId(), is(1));
+        assertThat(request.getBatch().toArray()[0], equalTo(entry1));
+        assertThat(request.getBatch().toArray()[1], equalTo(entry2));
+    }
+}
---
+++ core/api/src/test/java/org/onosproject/net/flowext/FlowRuleExtEntryTest.java
@@ -0,0 +1,27 @@
+package org.onosproject.net.flowext;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.onosproject.net.DeviceId;
+/** junit for FlowRuleExtEntry constructor*/
+public class FlowRuleExtEntryTest {
+
+    @Test
+	public void testConstructor() {
+    	String deviceId = "of:123456";
+        FlowRuleExtEntry entry = new FlowRuleExtEntry(DeviceId.deviceId(deviceId), deviceId.getBytes());
+        assertArrayEquals(deviceId.getBytes(), entry.getFlowEntryExt());
+        assertEquals(deviceId, entry.getDeviceId().toString());
+        assertEquals(deviceId.getBytes().length, entry.getLength());
+    }
+
+    @Test
+    public void testEquals() {
+        String deviceId = "of:123456";
+        FlowRuleExtEntry entry1 = new FlowRuleExtEntry(DeviceId.deviceId(deviceId), deviceId.getBytes());
+        FlowRuleExtEntry entry2 = new FlowRuleExtEntry(DeviceId.deviceId(deviceId), deviceId.getBytes());
+        assertEquals(entry1, entry2);
+    }
+
+}
