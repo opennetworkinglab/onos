17a18b21a563f36a02258d147503893d03604353
Bri Prebilic Cole <bri@onlab.us>
2015-01-19 17:15:59 -0800

    GUI -- Added some test files to practice Angular and link with the IconService. WIP
    Change-Id: Ifcacc107b99c6ff6c983c3d3821084947657dcd3

---
+++ web/gui/src/main/webapp/_bripc/show-icons-test.js
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2014,2015 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+ ONOS GUI -- Showing Icons Test Module
+
+ @author Bri Prebilic Cole
+ */
+
+(function () {
+    'use strict';
+
+    angular.module('showIconsTest', ['onosSvg'])
+
+        .controller('OvShowIconsTest', ['$log', 'GlyphService', 'IconService',
+            function ($log, gs, icns) {
+                var self = this;
+                self.a = 1;
+                self.b = 2;
+
+                self.message = "Hi";
+
+                $log.log('OvShowIconsTest has been created');
+            }]);
+})();
