a92f0cb2f5ca9920ce3fcf652239d506ac37565b
unknown <j00273701@X00308683A4.china.huawei.com>
2015-01-21 15:15:06 +0800

    use to test case2

--- core/store/serializers/src/main/java/org/onosproject/store/serializers/FlowRuleExtEntrySerializer.java
+++ core/store/serializers/src/main/java/org/onosproject/store/serializers/FlowRuleExtEntrySerializer.java
@@ -4,34 +4,35 @@ import org.onosproject.net.DeviceId;
 import org.onosproject.net.flowext.FlowRuleExtEntry;
 
 import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Registration;
 import com.esotericsoftware.kryo.Serializer;
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
 
 public class FlowRuleExtEntrySerializer extends Serializer<FlowRuleExtEntry> {
 
-	public FlowRuleExtEntrySerializer() {
-		super(false, true);
-		// TODO Auto-generated constructor stub
-	}
+        public FlowRuleExtEntrySerializer() {
+                super(false, true);
+               // TODO Auto-generated constructor stub
+        }
 
 	@Override
-	public FlowRuleExtEntry read(Kryo kryo, Input input,
-			Class<FlowRuleExtEntry> object) {
-		// TODO Auto-generated method stub
-		DeviceId deviceid = (DeviceId) kryo.readClassAndObject(input);
-		Class<?> classT = kryo.readClass(input);
-		int length = input.readInt();
-		byte[] buf = input.readBytes(length);
-		return new FlowRuleExtEntry(deviceid, classT, buf);
-	}
+        public FlowRuleExtEntry read(Kryo kryo, Input input,
+                         Class<FlowRuleExtEntry> object) {
+                // TODO Auto-generated method stub
+                DeviceId deviceid = (DeviceId) kryo.readClassAndObject(input);
+                Registration classT = kryo.readClass(input);
+                int length = input.readInt();
+                byte[] buf = input.readBytes(length);
+                return new FlowRuleExtEntry(deviceid, classT.getClass(), buf);
+        }
 
 	@Override
-	public void write(Kryo kyro, Output output, FlowRuleExtEntry object) {
-		// TODO Auto-generated method stub
-	        kyro.writeClassAndObject(output, object.getDeviceId());
-	        kyro.writeClass(output, object.getClassT());
-		output.writeInt(object.getFlowEntryExt().length);
-		output.writeBytes(object.getFlowEntryExt());
-	}
+        public void write(Kryo kyro, Output output, FlowRuleExtEntry object) {
+                // TODO Auto-generated method stub
+                kyro.writeClassAndObject(output, object.getDeviceId());
+                kyro.writeClass(output, object.getClassT());
+                output.writeInt(object.getFlowEntryExt().length);
+                output.writeBytes(object.getFlowEntryExt());
+        }
 }
