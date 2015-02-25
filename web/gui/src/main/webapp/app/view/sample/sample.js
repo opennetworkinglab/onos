439cd7794f5ca525d4dbc2b93dbcf23bc97b1dce
Bri Prebilic Cole <bri@onlab.us>
2015-02-25 09:20:31 -0800

    GUI -- Created sample button and toggle for demo.
    Change-Id: I3e2fc93f63688856ce14f347e20b4fb813866970

--- web/gui/src/main/webapp/app/view/sample/sample.js
+++ web/gui/src/main/webapp/app/view/sample/sample.js
@@ -20,25 +20,44 @@
 
 (function () {
     'use strict';
-    var tbs;
+    var $log, tbs, flash,
+        sampleDiv;
+
+    function btnFn() {
+        flash.flash('Hi there friends!');
+    }
+    function togFn(display) {
+        if (display) { sampleDiv.style('display', 'block'); }
+        else { sampleDiv.style('display', 'none'); }
+    }
 
     angular.module('ovSample', ['onosUtil'])
-        .controller('OvSampleCtrl', ['$log', 'ToolbarService',
-            function (_$log_, _tbs_) {
-                var self = this,
-                    $log = _$log_,
-                    tbs = _tbs_;
+        .controller('OvSampleCtrl', ['$log', 'ToolbarService', 'FlashService',
+            function (_$log_, _tbs_, _flash_) {
+                var self = this;
+                $log = _$log_;
+                tbs = _tbs_;
+                flash = _flash_;
 
                 self.message = 'Hey there folks!';
+
+                sampleDiv = d3.select('#ov-sample')
+                    .append('div')
+                    .text('Look at me!')
+                    .style({
+                        'display': 'none',
+                        'color': 'rgb(204, 89, 81)',
+                        'font-size': '20pt'
+                    });
 
                 var toolbar = tbs.createToolbar('sample'),
                     rset = [{ gid: 'switch', cb: function () {}},
                         { gid: 'bird', cb: function () {}}];
-                toolbar.addButton('some-btn', 'crown', function () {});
-                toolbar.addToggle('some-tog', 'chain', function () {});
+                toolbar.addButton('hello-btn', 'crown', btnFn);
+                toolbar.addToggle('sample-tog', 'chain', false, togFn);
                 toolbar.addSeparator();
                 toolbar.addRadioSet('some-rad', rset);
-                toolbar.show();
+                toolbar.hide();
 
              $log.log('OvSampleCtrl has been created');
         }]);
