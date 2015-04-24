f0e1faeabd32a28fccbd27d141b5a3c689c4c834
Thomas Vachuska <tom@onlab.us>
2015-04-24 00:51:51 -0700

    Sketching out basis for network configuration subsystem API.
    Change-Id: I9d56a8392bd300566fb333607102cc965b2fa66e

---
+++ exp/api/src/main/java/org/onosproject/exp/net/config/NetworkConfigRegistry.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright 2015 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onosproject.exp.net.config;
+
+import java.util.Set;
+
+/**
+ * Service for tracking network configuration factories.
+ */
+public interface NetworkConfigRegistry {
+
+    /**
+     * Registers the specified configuration factory.
+     *
+     * @param configFactory configuration factory
+     */
+    void registerConfigFactory(ConfigFactory configFactory);
+
+    /**
+     * Unregisters the specified configuration factory.
+     *
+     * @param configFactory configuration factory
+     */
+    void unregisterConfigFactory(ConfigFactory configFactory);
+
+    /**
+     * Returns set of configuration factories available for the specified
+     * class of subject.
+     *
+     * @param subjectClass subject class
+     */
+    <T> Set<ConfigFactory<T>> getConfigFactories(Class<T> subjectClass);
+
+    /**
+     * Returns the configuration type registered for the specified
+     * subject type and key.
+     *
+     * @param subjectClass subject class
+     */
+    <T> ConfigFactory<T> getConfigFactory(Class<T> subjectClass, String configKey);
+
+    /**
+     * Returns the configuration type registered for the specified
+     * configuration class.
+     *
+     * @param configClass configuration class
+     */
+    <T> ConfigFactory<T> getConfigFactory(Class<Config<T>> configClass);
+
+}
---
+++ exp/api/src/main/java/org/onosproject/exp/net/config/NetworkConfigService.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright 2015 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onosproject.exp.net.config;
+
+import java.util.Set;
+
+/**
+ * Service for tracking network configurations which specify how the discovered
+ * network information should be interpretted and how the network should be
+ * configured.
+ */
+public interface NetworkConfigService {
+
+    /**
+     * Returns the set of subjects for which some configuration is available.
+     *
+     * @param subjectClass subject class
+     * @return set of configured subjects
+     */
+    <T> Set<T> getSubjects(Class<T> subjectClass);
+
+    /**
+     * Returns the set of subjects for which the specified configuration is
+     * available.
+     *
+     * @param subjectClass subject class
+     * @param configClass  configuration class
+     * @return set of configured subjects
+     */
+    <T> Set<T> getSubjects(Class<T> subjectClass, Class<Config<T>> configClass);
+
+
+    /**
+     * Returns all configurations for the specified subject.
+     *
+     * @param subject configuration subject
+     * @return set of configurations
+     */
+    <T> Set<Config<T>> getConfigs(T subject);
+
+    /**
+     * Returns the configuration for the specified subject and configuration
+     * class if one is available; null otherwise.
+     *
+     * @param subject     configuration subject
+     * @param configClass configuration class
+     * @return configuration or null if one is not available
+     */
+    <T> Config<T> getConfig(T subject, Class<Config<T>> configClass);
+
+}
