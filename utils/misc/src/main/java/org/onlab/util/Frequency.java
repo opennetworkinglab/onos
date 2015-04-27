ea56028efdac4299ec6d2e8cc8de054dc2dddd79
Sho SHIMIZU <sshimizu@us.fujitsu.com>
2015-04-27 11:29:56 -0700

    Implement a class representing frequency
    This resolves ONOS-1769
    
    Change-Id: Ia4d0da59aec79bafdf7686dfad1f1952f94cefc7

---
+++ utils/misc/src/main/java/org/onlab/util/Frequency.java
@@ -0,0 +1,165 @@
+/*
+ * Copyright 2015 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onlab.util;
+
+import com.google.common.base.MoreObjects;
+
+import java.util.Objects;
+
+/**
+ * Class representing frequency. This class is intended to be used for a value whose unit is Hz
+ * and its family (KHz, MHz, etc.).
+ *
+ * <p>
+ * Note: this class is mainly intended to be used for lambda, which
+ * represents THz order. Long has enough space to represent over THz frequency as Hz,
+ * and the underlying value is long as Hz. This means this class can't represent
+ * sub-Hz accuracy.
+ * </p>
+ */
+public final class Frequency {
+
+    private static final long KHZ = 1_000L;
+    private static final long MHZ = 1_000_000L;
+    private static final long GHZ = 1_000_000_000L;
+    private static final long THZ = 1_000_000_000_000L;
+
+    private final long frequency;   // frequency in Hz
+
+    /**
+     * Creates an instance representing the specified frequency in Hz.
+     *
+     * @param frequency frequency in Hz
+     */
+    private Frequency(long frequency) {
+        this.frequency = frequency;
+    }
+
+    /**
+     * Return the value this instance represents as Hz.
+     *
+     * @return frequency in Hz
+     */
+    public long asHz() {
+        return frequency;
+    }
+
+    /**
+     * Returns an instance representing the specified value in Hz.
+     *
+     * @param value frequency in Hz
+     * @return instance representing the given frequency
+     */
+    public static Frequency ofHz(long value) {
+        return new Frequency(value);
+    }
+
+    /**
+     * Returns an instance representing the specified value in KHz.
+     *
+     * @param value frequency in KHz
+     * @return instance representing the given frequency
+     */
+    public static Frequency ofKHz(double value) {
+        return new Frequency((long) (value * KHZ));
+    }
+
+    /**
+     * Returns an instance representing the specified value in MHz.
+     *
+     * @param value frequency in MHz
+     * @return instance representing the given frequency
+     */
+    public static Frequency ofMHz(double value) {
+        return new Frequency((long) (value * MHZ));
+    }
+
+    /**
+     * Returns an instance representing the specified value in GHz.
+     *
+     * @param value frequency in GHz
+     * @return instance representing the given frequency
+     */
+    public static Frequency ofGHz(double value) {
+        return new Frequency((long) (value * GHZ));
+    }
+
+    /**
+     * Returns an instance representing the specified value in THz.
+     *
+     * @param value frequency in THz
+     * @return instance representing the given frequency
+     */
+    public static Frequency ofTHz(double value) {
+        return new Frequency((long) (value * THZ));
+    }
+
+    /**
+     * Returns a Frequency whose value is (this + value).
+     *
+     * @param value value to be added to this Frequency
+     * @return this + value
+     */
+    public Frequency add(Frequency value) {
+        return new Frequency(this.frequency + value.frequency);
+    }
+
+    /**
+     * Returns a Frequency whose value is (this - value).
+     *
+     * @param value value to be subtracted from this Frequency
+     * @return this - value
+     */
+    public Frequency subtract(Frequency value) {
+        return new Frequency(this.frequency - value.frequency);
+    }
+
+    /**
+     * Returns a Frequency whose value is (this * value).
+     *
+     * @param value value to be multiplied by this Frequency
+     * @return this * value
+     */
+    public Frequency multiply(long value) {
+        return new Frequency(this.frequency * value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(frequency);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (!(obj instanceof Frequency)) {
+            return false;
+        }
+
+        final Frequency other = (Frequency) obj;
+        return this.frequency == other.frequency;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("frequency", frequency + "Hz")
+                .toString();
+    }
+}
---
+++ utils/misc/src/test/java/org/onlab/util/FrequencyTest.java
@@ -0,0 +1,90 @@
+/*
+ * Copyright 2015 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onlab.util;
+
+import com.google.common.testing.EqualsTester;
+import org.junit.Test;
+import org.onlab.junit.ImmutableClassChecker;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class FrequencyTest {
+
+    private final Frequency frequency1 = Frequency.ofMHz(1000);
+    private final Frequency sameFrequency1 = Frequency.ofMHz(1000);
+    private final Frequency frequency2 = Frequency.ofGHz(1000);
+    private final Frequency sameFrequency2 = Frequency.ofGHz(1000);
+    private final Frequency moreSameFrequency2 = Frequency.ofTHz(1);
+    private final Frequency frequency3 = Frequency.ofTHz(193.1);
+    private final Frequency sameFrequency3 = Frequency.ofGHz(193100);
+
+    /**
+     * Tests immutability of Frequency.
+     */
+    @Test
+    public void testImmutability() {
+        ImmutableClassChecker.assertThatClassIsImmutable(Frequency.class);
+    }
+
+    /**
+     * Tests equality of Frequency instances.
+     */
+    @Test
+    public void testEquality() {
+        new EqualsTester()
+                .addEqualityGroup(frequency1, sameFrequency1)
+                .addEqualityGroup(frequency2, sameFrequency2, moreSameFrequency2)
+                .addEqualityGroup(frequency3, sameFrequency3)
+                .testEquals();
+    }
+
+    /**
+     * Tests add operation of two Frequencies.
+     */
+    @Test
+    public void testAdd() {
+        Frequency low = Frequency.ofMHz(100);
+        Frequency high = Frequency.ofGHz(1);
+        Frequency expected = Frequency.ofMHz(1100);
+
+        assertThat(low.add(high), is(expected));
+    }
+
+    /**
+     * Tests subtract operation of two Frequencies.
+     */
+    @Test
+    public void testSubtract() {
+        Frequency high = Frequency.ofGHz(1);
+        Frequency low = Frequency.ofMHz(100);
+        Frequency expected = Frequency.ofMHz(900);
+
+        assertThat(high.subtract(low), is(expected));
+    }
+
+    /**
+     * Tests multiply operation of Frequency.
+     */
+    @Test
+    public void testMultiply() {
+        Frequency frequency = Frequency.ofMHz(1000);
+        long factor = 5;
+        Frequency expected = Frequency.ofGHz(5);
+
+        assertThat(frequency.multiply(5), is(expected));
+    }
+}
