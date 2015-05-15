e031189c212a84e6c464dfa5b06ca2bf2ac4fe52
Bri Prebilic Cole <bri@onlab.us>
2015-05-15 11:27:15 -0700

    CORD GUI -- Skeleton view framework created, can navigate between views.
    Change-Id: I69942d17ed7fff77f531fdcbe97aa315bd90005e

---
+++ apps/demo/cord-gui/src/main/webapp/cord.js
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2015 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+(function () {
+    'use strict';
+
+    var modules = [
+            'ngRoute'
+        ],
+        viewIds = [
+            'login',
+            'home',
+            'user',
+            'sub'
+        ],
+        viewDependencies = [],
+        dependencies;
+
+    function capitalize(word) {
+        return word ? word[0].toUpperCase() + word.slice(1) : word;
+    }
+
+    viewIds.forEach(function (id) {
+        if (id) {
+            viewDependencies.push('cord' + capitalize(id));
+        }
+    });
+
+    dependencies = modules.concat(viewDependencies);
+
+    angular.module('cordGui', dependencies)
+        .config(['$routeProvider', function ($routeProvider) {
+            $routeProvider
+                .otherwise({
+                    redirectTo: '/login'
+                });
+
+            function viewCtrlName(vid) {
+                return 'Cord' + capitalize(vid) + 'Ctrl';
+            }
+
+            function viewTemplateUrl(vid) {
+                return 'app/view/' + vid + '/' + vid + '.html';
+            }
+
+            viewIds.forEach(function (vid) {
+                if (vid) {
+                    $routeProvider.when('/' + vid, {
+                        controller: viewCtrlName(vid),
+                        controllerAs: 'ctrl',
+                        templateUrl: viewTemplateUrl(vid)
+                    });
+                }
+            });
+        }])
+        .controller('CordCtrl', [function () {
+
+        }]);
+}());
