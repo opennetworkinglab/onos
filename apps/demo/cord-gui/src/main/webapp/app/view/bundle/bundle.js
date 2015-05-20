10570eb4a942500637b37961cb49d2462bf6830e
Bri Prebilic Cole <bri@onlab.us>
2015-05-20 16:23:52 -0700

    ONOS-1935 - CORD-GUI -- Nav bar updated, skeleton bundle view created.
    Change-Id: Ife6e6775274a35f2941208967665d2ac91f686b7

--- apps/demo/cord-gui/src/main/webapp/app/view/bundle/bundle.js
+++ apps/demo/cord-gui/src/main/webapp/app/view/bundle/bundle.js
@@ -17,13 +17,71 @@
 (function () {
     'use strict';
 
+    var $log, $resource;
+
+    var before = 'http://localhost:8080/rs/bundle/0',
+        after = 'http://localhost:8080/rs/bundle/1';
+
+    var basic = 'Basic Bundle',
+        family = 'Family Bundle',
+        current, which,
+        avScope;
+
+    function getAvailable(scope) {
+        var AvailableData, resource;
+
+        AvailableData = $resource(which);
+        resource = AvailableData.get({},
+            // success
+            function () {
+                scope.name = resource.bundle.name;
+                scope.funcs = resource.bundle.functions;
+            },
+            // error
+            function () {
+                $log.error('Problem with resource', resource);
+            });
+        $log.debug('Resource received:', resource);
+    }
+
     angular.module('cordBundle', [])
-        .controller('CordBundleCtrl', ['$log', '$scope',
-            function ($log, $scope) {
+        .controller('CordBundleCtrl', ['$log', '$scope', '$resource',
+            function (_$log_, $scope, _$resource_) {
+                var BundleData, resource;
                 $scope.page = 'bundle';
+                $log = _$log_;
+                $resource = _$resource_;
+
+                BundleData = $resource(after);
+                resource = BundleData.get({},
+                    // success
+                    function () {
+                        $scope.name = resource.bundle.name;
+                        current = $scope.name;
+                        $scope.funcs = resource.bundle.functions;
+
+                        which = (current === basic) ? after : before;
+                        getAvailable(avScope);
+                    },
+                    // error
+                    function () {
+                        $log.error('Problem with resource', resource);
+                    });
+                $log.debug('Resource received:', resource);
 
                 $log.debug('Cord Bundle Ctrl has been created.');
-        }]);
+        }])
 
-    // can have a directive here that uses templateUrl for editable and readonly
+        .controller('CordAvailable', ['$scope',
+            function ($scope) {
+                avScope = $scope;
+
+                $log.debug('Cord Available Ctrl has been created.');
+        }])
+
+        .directive('bundleAvailable', function () {
+            return {
+                templateUrl: 'app/view/bundle/available.html'
+            };
+        });
 }());
