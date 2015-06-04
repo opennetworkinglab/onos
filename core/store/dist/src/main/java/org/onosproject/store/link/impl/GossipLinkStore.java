6f90c59bb0bc185a648d3668de2e3135438392b2
Thomas Vachuska <tom@onlab.us>
2015-06-03 20:06:58 -0700

    Reduced a log level for link removal message.
    Change-Id: I8535d9dfe78a9042e76583d0f0fab8c3dabc00b7

--- core/store/dist/src/main/java/org/onosproject/store/link/impl/GossipLinkStore.java
+++ core/store/dist/src/main/java/org/onosproject/store/link/impl/GossipLinkStore.java
@@ -466,8 +466,8 @@ public class GossipLinkStore
         try {
             timestamp = deviceClockService.getTimestamp(dstDeviceId);
         } catch (IllegalStateException e) {
-            log.warn("Failed to remove link {}, was not the master", key);
-            //there are times when this is called before mastership
+            log.debug("Failed to remove link {}, was not the master", key);
+            // there are times when this is called before mastership
             // handoff correctly completes.
             return null;
         }
