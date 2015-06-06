48a91f77cbf65beded5a040454b1ccd32ac17bed
Marc De Leenheer <marc@onlab.us>
2015-06-05 23:35:41 -0700

    Inject and remove static routes.
    Change-Id: I8be1414978fd0b5dfb7186aeb86d100087502748

---
+++ apps/routing/src/main/java/org/onosproject/routing/cli/RemoveRouteCommand.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2015 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onosproject.routing.cli;
+
+import org.apache.karaf.shell.commands.Argument;
+import org.apache.karaf.shell.commands.Command;
+import org.onlab.packet.IpAddress;
+import org.onlab.packet.IpPrefix;
+import org.onlab.packet.MacAddress;
+import org.onosproject.cli.AbstractShellCommand;
+import org.onosproject.routing.FibEntry;
+import org.onosproject.routing.FibListener;
+import org.onosproject.routing.FibUpdate;
+import org.onosproject.routing.StaticRoutingService;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+@Command(scope = "onos", name = "remove-route", description = "Removes static route")
+public class RemoveRouteCommand extends AbstractShellCommand {
+    @Argument(index = 0, name = "prefix IP MAC",
+            description = "prefix nexthopIP nexthopMAC",
+            required = true, multiValued = true)
+    String[] fibEntryString = null;
+
+    @Override
+    protected void execute() {
+        StaticRoutingService routingService = get(StaticRoutingService.class);
+
+        if (fibEntryString.length < 3) {
+            return;
+        }
+
+        IpPrefix prefix = IpPrefix.valueOf(fibEntryString[0]);
+        IpAddress nextHopIp = IpAddress.valueOf(fibEntryString[1]);
+        MacAddress nextHopMac = MacAddress.valueOf(fibEntryString[2]);
+        FibEntry fibEntry = new FibEntry(prefix, nextHopIp, nextHopMac);
+        FibUpdate fibUpdate = new FibUpdate(FibUpdate.Type.DELETE, fibEntry);
+
+        FibListener fibListener = routingService.getFibListener();
+        fibListener.update(Collections.emptyList(), Arrays.asList(fibUpdate));
+    }
+}
