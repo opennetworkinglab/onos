e5e4ca2a0cd7ed4a2133c48e24ab35c7223ff285
Brian O'Connor <bocon@onlab.us>
2015-06-22 18:16:03 -0400

    Javadoc fixes in Versioned and TransactionManager
    Change-Id: If4a8245aabfadaba15261aa3a4c90c6632cb73a3

--- core/store/dist/src/main/java/org/onosproject/store/consistent/impl/TransactionManager.java
+++ core/store/dist/src/main/java/org/onosproject/store/consistent/impl/TransactionManager.java
@@ -59,6 +59,7 @@ public class TransactionManager {
      * Constructs a new TransactionManager for the specified database instance.
      *
      * @param database database
+     * @param mapBuilder map builder
      */
     public TransactionManager(Database database, ConsistentMapBuilder<Long, Transaction> mapBuilder) {
         this.database = checkNotNull(database, "database cannot be null");
