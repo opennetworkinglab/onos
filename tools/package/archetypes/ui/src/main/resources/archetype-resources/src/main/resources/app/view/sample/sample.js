e11ce5a54fe3a10630e1d11208219ad732d8e644
Simon Hunt <simon@onlab.us>
2015-07-21 12:11:04 -0700

    Updated GUI Archetype to implement a simple table view.
    Change-Id: I1e4faa457d7407f81b1926949a09e8c4570399e2

--- tools/package/archetypes/ui/src/main/resources/archetype-resources/src/main/resources/app/view/sample/sample.js
+++ tools/package/archetypes/ui/src/main/resources/archetype-resources/src/main/resources/app/view/sample/sample.js
@@ -2,14 +2,70 @@
 (function () {
     'use strict';
 
+    // injected refs
+    var $log, $scope, fs, mast, ps, wss;
+
+    // internal state
+    var selRow,
+        detailsPanel,
+        pStartY, pHeight,
+        wSize;
+
+    // constants
+    var topPadding = 20,
+
+        detailsReq = 'sampleDetailsRequest',
+        detailsResp = 'sampleDetailsResponse',
+        pName = 'item-details-panel',
+
+        propOrder = [ 'id', 'label', 'code'],
+        friendlyProps = [ 'Item ID', 'Item Label', 'Special Code' ];
+
+
+    function respDetailsCb(data) {
+        $scope.panelData = data.details;
+        $scope.$apply();
+    }
+
     angular.module('ovSample', [])
         .controller('OvSampleCtrl',
-        ['$log', '$scope',
+        ['$log', '$scope', 'TableBuilderService',
+            'FnService', 'WebSocketService',
 
-            function ($log, $scope) {
-                var self = this;
+            function (_$log_, _$scope_, tbs, _fs_, _wss_) {
+                $log = _$log_;
+                $scope = _$scope_;
+                fs = _fs_;
+                wss = _wss_;
 
-                self.msg = 'A message from our app...';
+                var handlers = {};
+
+                $scope.panelData = [];
+
+                function selCb($event, row) {
+                    selRow = angular.element($event.currentTarget);
+                    if ($scope.selId) {
+                        wss.sendEvent(detailsReq, { id: row.id });
+                    } else {
+                        $log.debug('need to hide details panel');
+                        //detailsPanel.hide()
+                    }
+                    $log.debug('Got a click on:', row);
+                }
+
+                tbs.buildTable({
+                    scope: $scope,
+                    tag: 'sample',
+                    selCb: selCb
+                });
+
+                // details response handler
+                handlers[detailsResp] = respDetailsCb;
+                wss.bindHandlers(handlers);
+
+                $scope.$on('$destroy', function () {
+                    wss.unbindHandlerse(handlers);
+                });
 
                 $log.log('OvSampleCtrl has been created');
             }]);
