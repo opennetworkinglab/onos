409b9cbf722887ec99012799f7f5844f3e39e9f4
Thomas Vachuska <tom@onlab.us>
2015-07-31 13:07:12 -0700

    Increasing tolerance for evicting latent listeners.
    Change-Id: I3ab62084479185160ef7cc8ba5035a34cb96ad15

--- core/net/src/main/java/org/onosproject/event/impl/CoreEventDispatcher.java
+++ core/net/src/main/java/org/onosproject/event/impl/CoreEventDispatcher.java
@@ -49,7 +49,7 @@ public class CoreEventDispatcher extends DefaultEventSinkRegistry
     private final Logger log = getLogger(getClass());
 
     // Default number of millis a sink can take to process an event.
-    private static final long DEFAULT_EXECUTE_MS = 2_000; // ms
+    private static final long DEFAULT_EXECUTE_MS = 5_000; // ms
     private static final long WATCHDOG_MS = 250; // ms
 
     private final BlockingQueue<Event> events = new LinkedBlockingQueue<>();
@@ -122,6 +122,8 @@ public class CoreEventDispatcher extends DefaultEventSinkRegistry
                         break;
                     }
                     process(event);
+                } catch (InterruptedException e) {
+                    log.warn("Dispatch loop interrupted");
                 } catch (Exception e) {
                     log.warn("Error encountered while dispatching event:", e);
                 }
