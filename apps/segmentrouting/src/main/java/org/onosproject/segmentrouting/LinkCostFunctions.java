9a10ec6833954298bf2434dc4dac23d09fa16a24
vishal <vishal.iitkgp08@gmail.com>
2015-08-06 18:21:25 +0000

    updated the link cost function

--- apps/segmentrouting/src/main/java/org/onosproject/segmentrouting/LinkCostFunctions.java
+++ apps/segmentrouting/src/main/java/org/onosproject/segmentrouting/LinkCostFunctions.java
@@ -25,6 +25,8 @@ import org.slf4j.LoggerFactory;
 public class LinkCostFunctions {
 
     private static Logger log = LoggerFactory.getLogger(LinkCostFunctions.class);
+    private HashMap<Link, LinkStatsService.LinkStats> prevLinkStatsMapper =
+                                    new HashMap<Link, LinkStatsService.LinkStats>();
 
     /* Returns folw rates for each link in unit of bytes/sec */
     public HashMap<Link, Double> flowRates(
@@ -33,11 +35,28 @@ public class LinkCostFunctions {
         HashMap<Link, Double> flowRatesMapper = new HashMap<Link, Double>();
 
         for (Link link : linkStatsMapper.keySet()) {
-            LinkStatsService.LinkStats linkStats = linkStatsMapper.get(link);
-            double rate = linkStats.bytesTransferred() /
-                            (linkStats.durationInNanosec() * 1e-9);
+            LinkStatsService.LinkStats linkStats = linkStatsMapper
+                                                   .get(link);
+            double rate = 0.0;
+            if (prevLinkStatsMapper.containsKey(link)) {
+                LinkStatsService.LinkStats prevLinkStats = prevLinkStatsMapper
+                                                           .get(link);
+                if (linkStats.durationInNanosec() - prevLinkStats.durationInNanosec() != 0) {
+                    rate = (linkStats.bytesTransferred() - prevLinkStats.bytesTransferred()) /
+                                (1e-9 * (linkStats.durationInNanosec() -
+                                         prevLinkStats.durationInNanosec()));
+                }
+            } else {
+                if (linkStats.durationInNanosec() != 0) {
+                    rate = linkStats.bytesTransferred() /
+                                (linkStats.durationInNanosec() * 1e-9);
+                }
+            }
             flowRatesMapper.put(link, rate);
         }
+        prevLinkStatsMapper.clear();
+        prevLinkStatsMapper.putAll(linkStatsMapper);
+
         return flowRatesMapper;
     }
 }
