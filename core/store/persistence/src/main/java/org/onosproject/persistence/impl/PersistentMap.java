92511f22a000bc92862128a9c6ce32e2b0ec7015
Aaron Kruglikov <aaron.kruglikov@us.fujitsu.com>
2015-10-12 14:39:04 -0700

    Creating persistence service.
    Change-Id: Ib78b4001a24c71b4096e5a2a446dbd5009aa1090

---
+++ core/store/persistence/src/main/java/org/onosproject/persistence/impl/PersistentMap.java
@@ -0,0 +1,192 @@
+/*
+ * Copyright 2015 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onosproject.persistence.impl;
+
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
+import org.mapdb.DB;
+import org.mapdb.Hasher;
+import org.onosproject.store.service.Serializer;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.Set;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+
+/**
+ * A map implementation that stores and receives all data from a serialized internal map.
+ */
+public class PersistentMap<K, V> implements Map<K, V> {
+
+    private final Serializer serializer;
+
+    private final org.mapdb.DB database;
+
+    private final Map<byte[], byte[]> items;
+
+    private final String name;
+
+    public PersistentMap(Serializer serializer, DB database, String name) {
+        this.serializer = checkNotNull(serializer);
+        this.database = checkNotNull(database);
+        this.name = checkNotNull(name);
+
+        items = database
+                .createHashMap(name)
+                .keySerializer(org.mapdb.Serializer.BYTE_ARRAY)
+                .valueSerializer(org.mapdb.Serializer.BYTE_ARRAY)
+                .hasher(Hasher.BYTE_ARRAY)
+                .makeOrGet();
+    }
+
+    /**
+     * Reads this set in deserialized form into the provided map.
+     *
+     * @param items the map to be populated
+     */
+    public void readInto(Map<K, V> items) {
+        this.items.forEach((keyBytes, valueBytes) ->
+                                   items.put(serializer.decode(keyBytes),
+                                             serializer.decode(valueBytes)));
+    }
+
+    @Override
+    public V remove(Object key) {
+        checkNotNull(key, "Key can not be null.");
+        V removed = get(key);
+        items.remove(serializer.encode(key));
+        return removed;
+    }
+
+    @Override
+    public int size() {
+        return items.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        checkNotNull(key, "Key cannot be null.");
+        return items.containsKey(serializer.encode(key));
+    }
+
+    @Override
+    public boolean containsValue(Object value) {
+        checkNotNull(value, "Value cannot be null.");
+        byte[] serialized = serializer.encode(value);
+        for (byte[] compareValue : items.values()) {
+            boolean same = true;
+            if (compareValue == null) {
+                same = false;
+            } else if (compareValue.length != serialized.length) {
+                same = false;
+            } else {
+                for (int i = 0; i < serialized.length; i++) {
+                    if (serialized[i] != compareValue[i]) {
+                        same = false;
+                        break;
+                    }
+                }
+            }
+            if (same) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public V get(Object key) {
+        checkNotNull(key, "Key cannot be null.");
+        return serializer.decode(items.get(serializer.encode(key)));
+    }
+
+    @Override
+    public V put(K key, V value) {
+        checkNotNull(key, "Key cannot be null.");
+        checkNotNull(value, "Value cannot be null.");
+        byte[] prevVal = items.put(serializer.encode(key), serializer.encode(value));
+        if (prevVal == null) {
+            return null;
+        }
+        return serializer.decode(prevVal);
+    }
+
+    @Override
+    public void putAll(Map<? extends K, ? extends V> m) {
+        checkNotNull(m, "The passed in map cannot be null.");
+        m.forEach((k, v) -> items.put(serializer.encode(k), serializer.encode(v)));
+    }
+
+    @Override
+    public void clear() {
+        items.clear();
+    }
+
+    @Override
+    public Set<K> keySet() {
+        Set<K> keys = Sets.newHashSet();
+        items.keySet().forEach(k -> keys.add(serializer.decode(k)));
+        return keys;
+    }
+
+    @Override
+    public Collection<V> values() {
+        Collection<V> values = Sets.newHashSet();
+        items.values().forEach(v -> values.add(serializer.decode(v)));
+        return values;
+    }
+
+    @Override
+    public Set<Entry<K, V>> entrySet() {
+        Set<Entry<K, V>> entries = Sets.newHashSet();
+        items.entrySet().
+                forEach(e -> entries.add(Maps.immutableEntry(serializer.decode(e.getKey()),
+                                                             serializer.decode(e.getValue()))));
+        return entries;
+    }
+
+    @Override
+    public boolean equals(Object map) {
+        //This is not threadsafe and on larger maps incurs a significant processing cost
+        if (!(map instanceof Map)) {
+            return false;
+        }
+        Map asMap = (Map) map;
+        if (this.size() != asMap.size()) {
+            return false;
+        }
+        for (Entry entry : this.entrySet()) {
+            Object key = entry.getKey();
+            if (!asMap.containsKey(key) || !asMap.get(key).equals(entry.getValue())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
---
+++ core/store/persistence/src/main/java/org/onosproject/persistence/impl/PersistentSet.java
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2015 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onosproject.persistence.impl;
+
+import com.google.common.collect.Iterators;
+import org.mapdb.DB;
+import org.mapdb.Hasher;
+import org.mapdb.Serializer;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Set;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+/**
+ * A set implementation that gets and receives all data from a serialized internal set.
+ */
+//TODO add locking for reads and writes
+public class PersistentSet<E> implements Set<E> {
+
+    private final org.onosproject.store.service.Serializer serializer;
+
+    private final org.mapdb.DB database;
+
+    private final Set<byte[]> items;
+
+    private final String name;
+
+    public PersistentSet(org.onosproject.store.service.Serializer serializer, DB database, String name) {
+        this.serializer = checkNotNull(serializer);
+        this.database = checkNotNull(database);
+        this.name = checkNotNull(name);
+
+        items = database
+                .createHashSet(name)
+                .serializer(Serializer.BYTE_ARRAY)
+                .hasher(Hasher.BYTE_ARRAY)
+                .makeOrGet();
+    }
+
+    public void readInto(Set<E> items) {
+        this.items.forEach(item -> items.add(serializer.decode(item)));
+    }
+
+    @Override
+    public int size() {
+        return items.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return items.isEmpty();
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        checkNotNull(o, "The argument cannot be null");
+        return items.contains(serializer.encode(o));
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return Iterators.transform(items.iterator(), serializer::decode);
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] retArray = new Object[items.size()];
+        int index = 0;
+        Iterator<byte[]> iterator = items.iterator();
+        while (iterator.hasNext()) {
+            retArray[index] = serializer.decode(iterator.next());
+            index++;
+        }
+        return retArray;
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        checkNotNull(a, "The passed in array cannot be null.");
+        int index = 0;
+        Iterator<byte[]> iterator = items.iterator();
+        T[] retArray;
+        if (a.length >= items.size()) {
+            retArray = a;
+        } else {
+            retArray = (T[]) new Object[items.size()];
+        }
+        while (iterator.hasNext()) {
+            retArray[index++] = serializer.decode(iterator.next());
+        }
+        if (retArray.length > items.size()) {
+            retArray[index] = null;
+        }
+        return retArray;
+    }
+
+    @Override
+    public boolean add(E item) {
+        checkNotNull("Item to be added cannot be null.");
+        return items.add(serializer.encode(item));
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        checkNotNull(o, "Item to be removed cannot be null.");
+        return items.remove(serializer.encode(o));
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        checkNotNull(c, "Collection cannot be internal.");
+        for (Object item : c) {
+            if (!items.contains(serializer.encode(item))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        checkNotNull(c, "The collection to be added cannot be null.");
+        boolean changed = false;
+        for (Object item : c) {
+            changed = items.add(serializer.encode(item)) || changed;
+        }
+        return changed;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        boolean changed = false;
+        for (byte[] item : items) {
+            E deserialized = serializer.decode(item);
+            if (!c.contains(deserialized)) {
+                changed = items.remove(item) || changed;
+            }
+        }
+        return changed;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean changed = false;
+        for (Object item : c) {
+            changed = items.remove(serializer.encode(item)) || changed;
+        }
+        return changed;
+    }
+
+    @Override
+    public void clear() {
+        items.clear();
+    }
+
+    @Override
+    public boolean equals(Object set) {
+        //This is not threadsafe and on larger sets incurs a significant processing cost
+        if (!(set instanceof Set)) {
+            return false;
+        }
+        Set asSet = (Set) set;
+        if (asSet.size() != this.size()) {
+            return false;
+        }
+        for (Object item : this) {
+            if (!asSet.contains(item)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
