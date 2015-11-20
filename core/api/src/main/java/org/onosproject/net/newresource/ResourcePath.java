opennetworkinglab/onos

commit c9546a3646423a92254708fb00a155669a8a1b2a

Refactor ResourcePath internal implementation
Change-Id: Idb3aa467b4d0e8181adf0d1766812a038b5408ac

core/api/src/main/java/org/onosproject/net/newresource/ResourcePath.java
@@ -20,6 +20,7 @@ import com.google.common.base.MoreObjects;
 import com.google.common.collect.ImmutableList;
 
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -39,17 +40,10 @@ import static com.google.common.base.Preconditions.checkNotNull;
 @Beta
 public final class ResourcePath {
 
-    private final List<Object> resources;
+    private final ResourcePath parent;
+    private final Object last;
 
     public static final ResourcePath ROOT = new ResourcePath(ImmutableList.of());
-
-    public static ResourcePath child(ResourcePath parent, Object child) {
-        ImmutableList<Object> components = ImmutableList.builder()
-                .addAll(parent.components())
-                .add(child)
-                .build();
-        return new ResourcePath(components);
-    }
 
     /**
      * Creates an resource path from the specified components.
@@ -67,13 +61,32 @@ public final class ResourcePath {
      */
     public ResourcePath(List<Object> components) {
         checkNotNull(components);
+        if (components.isEmpty()) {
+            this.parent = null;
+            this.last = null;
+            return;
+        }
 
-        this.resources = ImmutableList.copyOf(components);
+        LinkedList<Object> children = new LinkedList<>(components);
+        this.last = children.pollLast();
+        this.parent = new ResourcePath(children);
+    }
+
+    /**
+     * Creates an resource path from the specified parent and child.
+     *
+     * @param parent the parent of this resource
+     * @param last a child of the parent
+     */
+    public ResourcePath(ResourcePath parent, Object last) {
+        this.parent = checkNotNull(parent);
+        this.last = checkNotNull(last);
     }
 
     // for serialization
     private ResourcePath() {
-        this.resources = null;
+        this.parent = null;
+        this.last = null;
     }
 
     /**
@@ -82,7 +95,15 @@ public final class ResourcePath {
      * @return the components of this resource path
      */
     public List<Object> components() {
-        return resources;
+        LinkedList<Object> components = new LinkedList<>();
+
+        ResourcePath parentPath = parent;
+        while (parentPath != null) {
+            components.addFirst(last);
+            parentPath = parent.parent;
+        }
+
+        return components;
     }
 
     /**
@@ -93,20 +114,11 @@ public final class ResourcePath {
      * If there is no parent, empty instance will be returned.
      */
     public Optional<ResourcePath> parent() {
-        if (!isRoot()) {
-            return Optional.of(new ResourcePath(resources.subList(0, resources.size() - 1)));
-        }
-
-        return Optional.empty();
+        return Optional.ofNullable(parent);
     }
 
-    /**
-     * Returns true if the path represents root.
-     *
-     * @return true if the path represents root, false otherwise.
-     */
-    public boolean isRoot() {
-        return resources.size() == 0;
+    public ResourcePath child(Object child) {
+        return new ResourcePath(this, child);
     }
 
     /**
@@ -115,14 +127,13 @@ public final class ResourcePath {
      * @return the last component of this instance.
      * The return value is equal to the last object of {@code components()}.
      */
-    public Object lastComponent() {
-        int last = resources.size() - 1;
-        return resources.get(last);
+    public Object last() {
+        return last;
     }
 
     @Override
     public int hashCode() {
-        return resources.hashCode();
+        return Objects.hash(this.parent, this.last);
     }
 
     @Override
@@ -134,13 +145,15 @@ public final class ResourcePath {
             return false;
         }
         final ResourcePath that = (ResourcePath) obj;
-        return Objects.equals(this.resources, that.resources);
+        return Objects.equals(this.parent, that.parent)
+                && Objects.equals(this.last, that.last);
     }
 
     @Override
     public String toString() {
         return MoreObjects.toStringHelper(this)
-                .add("resources", resources)
+                .add("parent", parent)
+                .add("last", last)
                 .toString();
     }
 }

core/api/src/test/java/org/onosproject/net/newresource/ResourcePathTest.java
@@ -76,7 +76,7 @@ public class ResourcePathTest {
         LinkKey linkKey = LinkKey.linkKey(CP1_1, CP2_1);
         ResourcePath path = new ResourcePath(linkKey);
 
-        LinkKey child = (LinkKey) path.lastComponent();
+        LinkKey child = (LinkKey) path.last();
         assertThat(child, is(linkKey));
     }
 }

core/net/src/main/java/org/onosproject/net/intent/impl/compiler/MplsPathIntentCompiler.java
@@ -146,8 +146,8 @@ public class MplsPathIntentCompiler implements IntentCompiler<MplsPathIntent> {
 
     private Optional<MplsLabel> findMplsLabel(LinkKey link) {
         return resourceService.getAvailableResources(new ResourcePath(link)).stream()
-                .filter(x -> x.lastComponent() instanceof MplsLabel)
-                .map(x -> (MplsLabel) x.lastComponent())
+                .filter(x -> x.last() instanceof MplsLabel)
+                .map(x -> (MplsLabel) x.last())
                 .findFirst();
     }
 

core/net/src/main/java/org/onosproject/net/intent/impl/compiler/OpticalConnectivityIntentCompiler.java
@@ -183,7 +183,7 @@ public class OpticalConnectivityIntentCompiler implements IntentCompiler<Optical
         IndexedLambda minLambda = findFirstLambda(lambdas);
         List<ResourcePath> lambdaResources = path.links().stream()
                 .map(x -> new ResourcePath(linkKey(x.src(), x.dst())))
-                .map(x -> ResourcePath.child(x, minLambda))
+                .map(x -> x.child(minLambda))
                 .collect(Collectors.toList());
 
         List<ResourceAllocation> allocations = resourceService.allocate(intent.id(), lambdaResources);
@@ -198,8 +198,8 @@ public class OpticalConnectivityIntentCompiler implements IntentCompiler<Optical
         return links.stream()
                 .map(x -> new ResourcePath(linkKey(x.src(), x.dst())))
                 .map(resourceService::getAvailableResources)
-                .map(x -> Iterables.filter(x, r -> r.lastComponent() instanceof IndexedLambda))
-                .map(x -> Iterables.transform(x, r -> (IndexedLambda) r.lastComponent()))
+                .map(x -> Iterables.filter(x, r -> r.last() instanceof IndexedLambda))
+                .map(x -> Iterables.transform(x, r -> (IndexedLambda) r.last()))
                 .map(x -> (Set<IndexedLambda>) ImmutableSet.copyOf(x))
                 .reduce(Sets::intersection)
                 .orElse(Collections.emptySet());

core/net/src/main/java/org/onosproject/net/newresource/impl/ResourceManager.java
@@ -169,7 +169,7 @@ public final class ResourceManager extends AbstractListenerManager<ResourceEvent
         checkNotNull(children);
         checkArgument(!children.isEmpty());
 
-        List<ResourcePath> resources = Lists.transform(children, x -> ResourcePath.child(parent, x));
+        List<ResourcePath> resources = Lists.transform(children, parent::child);
         return store.register(resources);
     }
 
@@ -179,7 +179,7 @@ public final class ResourceManager extends AbstractListenerManager<ResourceEvent
         checkNotNull(children);
         checkArgument(!children.isEmpty());
 
-        List<ResourcePath> resources = Lists.transform(children, x -> ResourcePath.child(parent, x));
+        List<ResourcePath> resources = Lists.transform(children, parent::child);
         return store.unregister(resources);
     }
 

core/net/src/test/java/org/onosproject/net/intent/impl/compiler/MockResourceService.java
@@ -90,7 +90,7 @@ class MockResourceService implements ResourceService {
 
     @Override
     public Collection<ResourcePath> getAvailableResources(ResourcePath parent) {
-        ResourcePath resource = ResourcePath.child(parent, MplsLabel.mplsLabel(10));
+        ResourcePath resource = parent.child(MplsLabel.mplsLabel(10));
         return ImmutableList.of(resource);
     }
 

core/store/dist/src/main/java/org/onosproject/store/newresource/impl/ConsistentResourceStore.java
@@ -264,7 +264,7 @@ public class ConsistentResourceStore extends AbstractStore<ResourceEvent, Resour
         }
 
         return children.value().stream()
-                .filter(x -> x.lastComponent().getClass().equals(cls))
+                .filter(x -> x.last().getClass().equals(cls))
                 .filter(consumerMap::containsKey)
                 .collect(Collectors.toList());
     }
@@ -344,7 +344,7 @@ public class ConsistentResourceStore extends AbstractStore<ResourceEvent, Resour
      */
     private boolean isRegistered(TransactionalMap<ResourcePath, List<ResourcePath>> map, ResourcePath resource) {
         // root is always regarded to be registered
-        if (resource.isRoot()) {
+        if (!resource.parent().isPresent()) {
             return true;
         }
 

commit 63ff35ab49c1920be718a7c8c7d7938f1a9ddaa9

Temporarily commenting out the device and link resource service listeners
There was bug introduced with a refactoring in commit:
c9546a3646423a92254708fb00a155669a8a1b2a

The code was refactored again in this commit:
60ac58efe977e2d8822657c013e724523cae32ac

Change-Id: I6c96dc1b9e0632865ae6742ed596624f7ed3d623

core/api/src/main/java/org/onosproject/net/newresource/ResourcePath.java
@@ -18,6 +18,7 @@ package org.onosproject.net.newresource;
 import com.google.common.annotations.Beta;
 import com.google.common.base.MoreObjects;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 
 import java.util.LinkedList;
 import java.util.List;

core/net/src/main/java/org/onosproject/net/newresource/impl/ResourceRegistrar.java
@@ -59,15 +59,17 @@ public final class ResourceRegistrar {
 
     @Activate
     public void activate() {
-        deviceListener = new ResourceDeviceListener(adminService, executor);
-        deviceService.addListener(deviceListener);
-        linkListener = new ResourceLinkListener(adminService, driverService, executor);
-        linkService.addListener(linkListener);
+        // FIXME there is a loop causing high resource utilization on device registration
+//        deviceListener = new ResourceDeviceListener(adminService, executor);
+//        deviceService.addListener(deviceListener);
+//        linkListener = new ResourceLinkListener(adminService, driverService, executor);
+//        linkService.addListener(linkListener);
     }
 
     @Deactivate
     public void deactivate() {
-        deviceService.removeListener(deviceListener);
-        linkService.removeListener(linkListener);
+        // FIXME there is a loop causing high resource utilization on device registration
+//        deviceService.removeListener(deviceListener);
+//        linkService.removeListener(linkListener);
     }
 }