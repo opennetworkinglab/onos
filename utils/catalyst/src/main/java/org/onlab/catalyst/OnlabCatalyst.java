a6b2e92e0f3091c11177b3972e87edf80a320d5d
Aaron Kruglikov <aaron.kruglikov@us.fujitsu.com>
2015-11-24 13:56:05 -0800

    fixing bundling
    Change-Id: I11bd416e9b5e2c6e8dd25bc485c3ea210ba7fadb

---
+++ utils/catalyst/src/main/java/org/onlab/catalyst/OnlabCatalyst.java
@@ -0,0 +1,7 @@
+package org.onlab.catalyst;
+
+/**
+ * Created by admin on 11/24/15.
+ */
+public class OnlabCatalyst {
+}
---
+++ utils/catalyst/src/main/java/org/onlab/catalyst/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Created by admin on 11/24/15.
+ */
+package org.onlab.catalyst;
