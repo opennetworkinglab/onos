07d80383de888969de3d629ceb161f440349391b
Aaron Kruglikov <aaron.kruglikov@us.fujitsu.com>
2015-12-07 16:54:37 -0800

    Rebuilding K Paths algorithm
    Change-Id: I8f5223cd3004ec89b203b1f177cd226911426906

---
+++ utils/misc/src/main/java/org/onlab/graph/KShortestPathsSearch.java
@@ -0,0 +1,194 @@
+/*
+ * Copyright 2015 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onlab.graph;
+
+import com.google.common.collect.ImmutableSet;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import org.slf4j.Logger;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static com.google.common.base.Preconditions.checkArgument;
+import static com.google.common.base.Preconditions.checkNotNull;
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * Runs K shortest paths algorithm on a provided directed graph.  Returns results in the form of an
+ * InnerOrderedResult so iteration through the returned paths will return paths in ascending order according to the
+ * provided EdgeWeight.
+ */
+public class KShortestPathsSearch<V extends Vertex, E extends Edge<V>> extends AbstractGraphPathSearch<V, E> {
+
+    private final Logger log = getLogger(getClass());
+
+    @Override
+    public Result<V, E> search(Graph<V, E> graph, V src, V dst, EdgeWeight<V, E> weight, int maxPaths) {
+        checkNotNull(src);
+        checkNotNull(dst);
+        //The modified edge weight removes any need to modify the original graph
+        InnerEdgeWeighter modifiedWeighter = new InnerEdgeWeighter(checkNotNull(weight));
+        checkArgument(maxPaths > 0);
+        Graph<V, E> originalGraph = checkNotNull(graph);
+        //the result contains the set of eventual results
+        InnerOrderedResult result = new InnerOrderedResult(src, dst, maxPaths);
+        ArrayList<Path<V, E>> resultPaths = new ArrayList<>(maxPaths);
+        ArrayList<Path<V, E>> potentialPaths = Lists.newArrayList();
+
+        DijkstraGraphSearch<V, E> dijkstraSearch = new DijkstraGraphSearch<>();
+        Set<Path<V, E>> dijkstraResults = dijkstraSearch.search(originalGraph, src, dst, modifiedWeighter, 1).paths();
+        //Checks if the dst was reachable
+        if (dijkstraResults.size() == 0) {
+            log.warn("No path was found.");
+            return result;
+        }
+        //If it was reachable adds the first shortest path to the set of results
+        resultPaths.add(dijkstraResults.iterator().next());
+
+        for (int k = 1; k < maxPaths; k++) {
+
+            for (int i = 0; i < (resultPaths.get(k - 1).edges().size() - 1); i++) {
+                V spurNode = resultPaths.get(k - 1).edges().get(i).src();
+                List<E> rootPathEdgeList = resultPaths.get(k - 1).edges().subList(0, i);
+
+                for (Path<V, E> path : resultPaths) {
+                    if (edgeListsAreEqual(rootPathEdgeList, path.edges().subList(0, i))) {
+                        modifiedWeighter.removedEdges.add(path.edges().get(i));
+                    }
+                }
+
+                //Effectively remove all nodes from the source path
+                for (E edge : rootPathEdgeList) {
+                    originalGraph.getEdgesFrom(edge.src()).forEach(e -> modifiedWeighter.removedEdges.add(e));
+                    originalGraph.getEdgesTo(edge.src()).forEach(e -> modifiedWeighter.removedEdges.add(e));
+                }
+
+                dijkstraResults = dijkstraSearch.search(originalGraph, spurNode, dst, modifiedWeighter, 1).paths();
+                if (dijkstraResults.size() != 0) {
+                    Path<V, E> spurPath = dijkstraResults.iterator().next();
+                    List<E> totalPath = new ArrayList<>(rootPathEdgeList);
+                    spurPath.edges().forEach(e -> totalPath.add(e));
+                    //The following line must use the original weighter not the modified weighter because the modified
+                    //weighter will count -1 values used for modifying the graph and return an inaccurate cost.
+                    potentialPaths.add(new DefaultPath<V, E>(totalPath,
+                                                             calculatePathCost(weight, totalPath)));
+                }
+
+                //Restore all removed paths and nodes
+                modifiedWeighter.removedEdges.clear();
+            }
+            if (potentialPaths.isEmpty()) {
+                break;
+            }
+            potentialPaths.sort(new InnerPathComparator());
+            resultPaths.add(potentialPaths.get(0));
+            potentialPaths.remove(0);
+        }
+        result.pathSet.addAll(resultPaths);
+
+        return result;
+    }
+    //Edge list equality is judges by shared endpoints, and shared endpoints should be the same
+    private boolean edgeListsAreEqual(List<E> edgeListOne, List<E> edgeListTwo) {
+        if (edgeListOne.size() != edgeListTwo.size()) {
+            return false;
+        }
+        E edgeOne;
+        E edgeTwo;
+        for (int i = 0; i < edgeListOne.size(); i++) {
+            edgeOne = edgeListOne.get(i);
+            edgeTwo = edgeListTwo.get(i);
+            if (!edgeOne.equals(edgeTwo)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private Double calculatePathCost(EdgeWeight<V, E> weighter, List<E> edges) {
+        Double totalCost = 0.0;
+        for (E edge : edges) {
+            totalCost += weighter.weight(edge);
+        }
+        return totalCost;
+    }
+
+    /**
+     * Weights edges to make them inaccessible if set, otherwise returns the result of the original EdgeWeight.
+     */
+    private class InnerEdgeWeighter implements EdgeWeight<V, E> {
+
+        private Set<E> removedEdges = Sets.newConcurrentHashSet();
+        private EdgeWeight<V, E> innerEdgeWeight;
+
+        public InnerEdgeWeighter(EdgeWeight<V, E> weight) {
+            this.innerEdgeWeight = weight;
+        }
+
+        @Override
+        public double weight(E edge) {
+            if (removedEdges.contains(edge)) {
+                //THIS RELIES ON THE LOCAL DIJKSTRA ALGORITHM AVOIDING NEGATIVES
+                return -1;
+            } else {
+                return innerEdgeWeight.weight(edge);
+            }
+        }
+    }
+
+    /**
+     * A result modified to return paths ordered according to the provided comparator.
+     */
+    protected class InnerOrderedResult extends DefaultResult {
+
+        private TreeSet<Path<V, E>> pathSet = new TreeSet<>(new InnerPathComparator());
+
+        public InnerOrderedResult(V src, V dst) {
+            super(src, dst);
+        }
+
+        public InnerOrderedResult(V src, V dst, int maxPaths) {
+            super(src, dst, maxPaths);
+        }
+
+        @Override
+        public Set<Path<V, E>> paths() {
+            return ImmutableSet.copyOf(pathSet);
+        }
+    }
+
+    /**
+     * Provides a comparator to order the set of paths.
+     */
+    private class InnerPathComparator implements Comparator<Path<V, E>> {
+
+        @Override
+        public int compare(Path<V, E> pathOne, Path<V, E> pathTwo) {
+            int comparisonValue = Double.compare(pathOne.cost(), pathTwo.cost());
+            if  (comparisonValue != 0) {
+                return comparisonValue;
+            } else if (edgeListsAreEqual(pathOne.edges(), pathTwo.edges())) {
+                return 0;
+            } else {
+                return 1;
+            }
+        }
+    }
+}
---
+++ utils/misc/src/test/java/org/onlab/graph/KShortestPathsSearchTest.java
@@ -0,0 +1,144 @@
+package org.onlab.graph;
+
+import com.google.common.collect.Lists;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import static com.google.common.collect.ImmutableSet.of;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Class for test KshortestPathsSearch.
+ */
+public class KShortestPathsSearchTest<V extends Vertex, E extends Edge<V>> extends GraphTest {
+    private KShortestPathsSearch<TestVertex, TestEdge> kShortestPathsSearch = new KShortestPathsSearch<>();
+    private GraphPathSearch.Result<TestVertex, TestEdge> result;
+
+    @Before
+    public void setUp() {
+        graph = new AdjacencyListsGraph<>(vertexes(), edges());
+    }
+    @Test
+    public void noPath() {
+        graph = new AdjacencyListsGraph<>(of(A, B, C, D),
+                                          of(new TestEdge(A, B, 1),
+                                             new TestEdge(B, A, 1),
+                                             new TestEdge(C, D, 1),
+                                             new TestEdge(D, C, 1)));
+        KShortestPathsSearch<TestVertex, TestEdge> kShortestPathsSearch = new KShortestPathsSearch<>();
+        GraphPathSearch.Result<TestVertex, TestEdge> result = kShortestPathsSearch.search(graph, A, D, weight, 1);
+        Set<Path<TestVertex, TestEdge>> resultPathSet = result.paths();
+        assertTrue("There should not be any paths.", resultPathSet.isEmpty());
+    }
+
+    @Test
+    public void testSinglePath() {
+        //Tests that there is only a single path possible between A and B
+        graph = new AdjacencyListsGraph<>(vertexes(), edges());
+        this.result = kShortestPathsSearch.search(graph, A, B, weight, 2);
+        Iterator<Path<TestVertex, TestEdge>> itr = result.paths().iterator();
+        assertEquals("incorrect paths count", 1, result.paths().size());
+        List<TestEdge> correctEdgeList = Lists.newArrayList();
+        correctEdgeList.add(new TestEdge(A, B, 1));
+        assertTrue("That wrong path was returned.",
+                   edgeListsAreEqual(correctEdgeList, result.paths().iterator().next().edges()));
+    }
+
+    @Test
+    public void testTwoPath() {
+        //Tests that there are only two paths between A and C and that they are returned in the correct order
+        result = kShortestPathsSearch.search(graph, A, C, weight, 3);
+        assertTrue("There are an unexpected number of paths.", result.paths().size() == 2);
+        Iterator<Path<TestVertex, TestEdge>> edgeListIterator = result.paths().iterator();
+        List<TestEdge> correctEdgeList = Lists.newArrayList();
+        correctEdgeList.add(new TestEdge(A, B, 1));
+        correctEdgeList.add(new TestEdge(B, C, 1));
+        assertTrue("The first path from A to C was incorrect.",
+                   edgeListsAreEqual(edgeListIterator.next().edges(), correctEdgeList));
+        correctEdgeList.clear();
+        correctEdgeList.add(new TestEdge(A, C, 3));
+        assertTrue("The second path from A to C was incorrect.",
+                   edgeListsAreEqual(edgeListIterator.next().edges(), correctEdgeList));
+    }
+
+    @Test
+    public void testFourPath() {
+        //Tests that there are only four paths between A and E and that they are returned in the correct order
+        //Also tests the special case where some correct solutions are equal
+        result = kShortestPathsSearch.search(graph, A, E, weight, 5);
+        assertTrue("There are an unexpected number of paths.", result.paths().size() == 4);
+        Iterator<Path<TestVertex, TestEdge>> edgeListIterator = result.paths().iterator();
+        List<TestEdge> correctEdgeList = Lists.newArrayList();
+        correctEdgeList.add(new TestEdge(A, B, 1));
+        correctEdgeList.add(new TestEdge(B, C, 1));
+        correctEdgeList.add(new TestEdge(C, E, 1));
+        assertTrue("The first path from A to E was incorrect.",
+                   edgeListsAreEqual(edgeListIterator.next().edges(), correctEdgeList));
+        correctEdgeList.clear();
+        //There are two paths of equal length that should hold positions two and three
+        List<TestEdge> alternateCorrectEdgeList = Lists.newArrayList();
+        correctEdgeList.add(new TestEdge(A, C, 3));
+        correctEdgeList.add(new TestEdge(C, E, 1));
+        alternateCorrectEdgeList.add(new TestEdge(A, B, 1));
+        alternateCorrectEdgeList.add(new TestEdge(B, D, 2));
+        alternateCorrectEdgeList.add(new TestEdge(D, E, 1));
+        List<TestEdge> candidateOne = edgeListIterator.next().edges();
+        List<TestEdge> candidateTwo = edgeListIterator.next().edges();
+        if (candidateOne.size() == 2) {
+            assertTrue("The second path from A to E was incorrect.",
+                       edgeListsAreEqual(candidateOne, correctEdgeList));
+            assertTrue("The third path from A to E was incorrect.",
+                       edgeListsAreEqual(candidateTwo, alternateCorrectEdgeList));
+        } else {
+            assertTrue("The second path from A to E was incorrect.",
+                       edgeListsAreEqual(candidateOne, alternateCorrectEdgeList));
+            assertTrue("The third path from A to E was incorrect.",
+                       edgeListsAreEqual(candidateTwo, correctEdgeList));
+        }
+        correctEdgeList.clear();
+        correctEdgeList.add(new TestEdge(A, B, 1));
+        correctEdgeList.add(new TestEdge(B, E, 4));
+        assertTrue("The fourth path rom A to E was incorrect",
+                   edgeListsAreEqual(edgeListIterator.next().edges(), correctEdgeList));
+
+    }
+
+    @Test
+    public void testPathsFromSink() {
+        //H is a sink in this topology, insure there are no paths from it to any other location
+        for (TestVertex vertex : vertexes()) {
+            assertTrue("There should be no paths from vertex H to any other node.",
+                       kShortestPathsSearch.search(graph, H, vertex, weight, 1).paths().size() == 0);
+        }
+    }
+
+    @Test
+    public void testLimitPathSetSize() {
+        //Checks to make sure that no more than K paths are returned
+        result = kShortestPathsSearch.search(graph, A, E, weight, 3);
+        assertTrue("There are an unexpected number of paths.", result.paths().size() == 3);
+        result = kShortestPathsSearch.search(graph, A, G, weight, 1);
+        assertTrue("There are an unexpected number of paths.", result.paths().size() == 1);
+    }
+
+    private boolean edgeListsAreEqual(List<TestEdge> edgeListOne, List<TestEdge> edgeListTwo) {
+        if (edgeListOne.size() != edgeListTwo.size()) {
+            return false;
+        }
+        TestEdge edgeOne;
+        TestEdge edgeTwo;
+        for (int i = 0; i < edgeListOne.size(); i++) {
+            edgeOne = edgeListOne.get(i);
+            edgeTwo = edgeListTwo.get(i);
+            if (!edgeOne.equals(edgeTwo)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
