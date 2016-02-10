60e739836c793547a19dbaf0037640b115923996
Shashikanth VH <shashikanth.vh@huawei.com>
2016-02-10 11:25:34 +0530

    [ONOS-3857] BGP sourceprefix packetlength protocol flow specification components.
    Change-Id: I4a8a209c07db954bb7fb3dcbc236bc3a8018a4b4

---
+++ protocols/bgp/bgpio/src/main/java/org/onosproject/bgpio/types/BgpFsSourcePrefix.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright 2016 Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onosproject.bgpio.types;
+
+import java.nio.ByteBuffer;
+import java.util.Objects;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.onlab.packet.IpPrefix;
+import org.onosproject.bgpio.exceptions.BgpParseException;
+import org.onosproject.bgpio.util.Constants;
+
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Preconditions;
+
+/**
+ * Provides implementation of IPv4AddressTlv.
+ */
+public class BgpFsSourcePrefix implements BgpValueType {
+
+    public static final byte FLOW_SPEC_TYPE = Constants.BGP_FLOWSPEC_SRC_PREFIX;
+    private byte length;
+    private IpPrefix ipPrefix;
+
+    /**
+     * Constructor to initialize parameters.
+     *
+     * @param length length of the prefix
+     * @param ipPrefix ip prefix
+     */
+    public BgpFsSourcePrefix(byte length, IpPrefix ipPrefix) {
+        this.ipPrefix = Preconditions.checkNotNull(ipPrefix);
+        this.length = length;
+    }
+
+    /**
+     * Returns ip prefix.
+     *
+     * @return ipPrefix ip prefix
+     */
+    public IpPrefix ipPrefix() {
+        return ipPrefix;
+    }
+
+    @Override
+    public short getType() {
+        return this.FLOW_SPEC_TYPE;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ipPrefix);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj instanceof BgpFsSourcePrefix) {
+            BgpFsSourcePrefix other = (BgpFsSourcePrefix) obj;
+            return Objects.equals(this.ipPrefix, other.ipPrefix) && Objects.equals(this.length, other.length);
+        }
+        return false;
+    }
+
+    @Override
+    public int write(ChannelBuffer cb) {
+        int iLenStartIndex = cb.writerIndex();
+        cb.writeByte(FLOW_SPEC_TYPE);
+        cb.writeByte(length);
+        cb.writeInt(ipPrefix.getIp4Prefix().address().toInt());
+        return cb.writerIndex() - iLenStartIndex;
+    }
+
+    /**
+     * Reads the channel buffer and returns object of IPv4AddressTlv.
+     *
+     * @param cb channelBuffer
+     * @param type address type
+     * @return object of flow spec source prefix
+     * @throws BgpParseException while parsing BgpFsSourcePrefix
+     */
+    public static BgpFsSourcePrefix read(ChannelBuffer cb, short type) throws BgpParseException {
+        return null;
+    }
+
+    /**
+     * Returns object of this class with specified values.
+     *
+     * @param ipPrefix ip prefix
+     * @param length length of ip prefix
+     * @return object of this class
+     */
+    public static BgpFsSourcePrefix of(final IpPrefix ipPrefix, final byte length) {
+        return new BgpFsSourcePrefix(length, ipPrefix);
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if (this.equals(o)) {
+            return 0;
+        }
+
+        if (o instanceof BgpFsSourcePrefix) {
+            BgpFsSourcePrefix that = (BgpFsSourcePrefix) o;
+
+            if (this.ipPrefix().prefixLength() == that.ipPrefix().prefixLength()) {
+                ByteBuffer value1 = ByteBuffer.wrap(this.ipPrefix.address().toOctets());
+                ByteBuffer value2 = ByteBuffer.wrap(that.ipPrefix.address().toOctets());
+                return value1.compareTo(value2);
+            }
+
+            if (this.ipPrefix().prefixLength() > that.ipPrefix().prefixLength()) {
+                return 1;
+            } else if (this.ipPrefix().prefixLength() < that.ipPrefix().prefixLength()) {
+                return -1;
+            }
+        }
+        return 1;
+    }
+
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(getClass())
+                .add("FLOW_SPEC_TYPE", FLOW_SPEC_TYPE).add("length", length)
+                .add("ipPrefix", ipPrefix).toString();
+    }
+}
