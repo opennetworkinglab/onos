ea32708ba865673f532d74fa4bfa42900da774a4
Simon Hunt <simon@onlab.us>
2016-04-11 16:49:07 -0700

    Temporary patch around unavailable username during websocket failover.
    Change-Id: I38f98816e2e9022d07aad49e1184faa2c2174428

--- web/gui/src/main/java/org/onosproject/ui/impl/UiWebSocketServlet.java
+++ web/gui/src/main/java/org/onosproject/ui/impl/UiWebSocketServlet.java
@@ -23,6 +23,7 @@ import org.onlab.osgi.ServiceDirectory;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
+import java.security.Principal;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
@@ -72,13 +73,18 @@ public class UiWebSocketServlet extends WebSocketServlet {
         }
 
         // FIXME: Replace this with globally shared opaque token to allow secure failover
-        String userName = request.getUserPrincipal().getName();
+        Principal p = request.getUserPrincipal();
+        String userName = p != null ? p.getName() : FAKE_USERNAME;
+
         UiWebSocket socket = new UiWebSocket(directory, userName);
         synchronized (sockets) {
             sockets.add(socket);
         }
         return socket;
     }
+
+    // FIXME: This should not be necessary
+    private static final String FAKE_USERNAME = "UI-user";
 
     /**
      * Sends the specified message to all the GUI clients.
