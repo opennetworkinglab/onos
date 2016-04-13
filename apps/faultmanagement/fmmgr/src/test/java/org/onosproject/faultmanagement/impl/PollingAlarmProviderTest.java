637c807aaed09777c09e857047c2ee3c17f2965f
Andrea Campanella <andrea@onlab.us>
2016-04-12 17:29:17 -0700

    Moving Polling alarm provider into faultamangement application
    Change-Id: Ia283ea39d22b727ba90b537c3efa9540fa5dbd99

--- apps/faultmanagement/fmmgr/src/test/java/org/onosproject/faultmanagement/impl/PollingAlarmProviderTest.java
+++ apps/faultmanagement/fmmgr/src/test/java/org/onosproject/faultmanagement/impl/PollingAlarmProviderTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Open Networking Laboratory
+ * Copyright 2016-present Open Networking Laboratory
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package org.onosproject.incubator.net.faultmanagement.alarm.impl;
+package org.onosproject.faultmanagement.impl;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
