6602db864b259275fb346120e4a60408fa87214f
Matteo Scandolo <teo@onlab.us>
2016-04-19 18:12:45 -0700

    Improved test environment
    Change-Id: Ic8dbdccbaeafabf99400e74f9d9630b447794a95

--- web/gui/src/main/webapp/tests/karma.conf.js
+++ web/gui/src/main/webapp/tests/karma.conf.js
@@ -58,7 +58,7 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ['mocha'],
 
 
     // web server port
@@ -80,7 +80,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
