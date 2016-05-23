c79ca54c8643b1e980108c73e39aabf95dac1541
Naoki Shiota <n-shiota@ay.jp.nec.com>
2016-05-23 11:24:26 -0700

    Deprecated OpticalLinkProvider.
    Change-Id: Iabfcfc676c0da193a2529f094ad02d3d0ac2509f

--- apps/optical/src/main/java/org/onosproject/optical/OpticalLinkProvider.java
+++ apps/optical/src/main/java/org/onosproject/optical/OpticalLinkProvider.java
@@ -46,7 +46,10 @@ import static org.onosproject.net.Link.Type.OPTICAL;
 /**
  * Ancillary provider to activate/deactivate optical links as their respective
  * devices go online or offline.
+ *
+ * @deprecated in Goldeneye (1.6.0)
  */
+@Deprecated
 @Component(immediate = true)
 public class OpticalLinkProvider extends AbstractProvider implements LinkProvider {
 
