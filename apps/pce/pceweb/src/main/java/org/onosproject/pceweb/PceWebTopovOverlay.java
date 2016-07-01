4bf9f2d369424b943e74abf0e8fde0816782362c
MaheshRaju-Huawei <mahesh.somalaraju@huawei.com>
2016-07-01 19:03:18 +0530

    CherryPick(1.6) PCE WEB GUI defect fixes
    Change-Id: I76e5323e382b6d2f76b7642009f866e6ba2334f2

--- apps/pce/pceweb/src/main/java/org/onosproject/pceweb/PceWebTopovOverlay.java
+++ apps/pce/pceweb/src/main/java/org/onosproject/pceweb/PceWebTopovOverlay.java
@@ -21,24 +21,24 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import org.onlab.util.Bandwidth;
 import org.onosproject.net.AnnotationKeys;
 import org.onosproject.net.Annotations;
 import org.onosproject.net.Device;
 import org.onosproject.net.DeviceId;
 import org.onosproject.net.Link;
+import org.onosproject.net.resource.ContinuousResource;
+import org.onosproject.net.resource.ResourceService;
+import org.onosproject.net.resource.Resource;
+import org.onosproject.net.resource.DiscreteResource;
+import org.onosproject.net.resource.Resources;
 import org.onosproject.ui.UiTopoOverlay;
 import org.onosproject.ui.topo.PropertyPanel;
 import org.onosproject.net.device.DeviceService;
 import org.onosproject.net.link.LinkEvent;
-import org.onosproject.net.resource.ContinuousResource;
-import org.onosproject.net.resource.DiscreteResource;
-import org.onosproject.net.resource.Resource;
-import org.onosproject.net.resource.ResourceService;
-import org.onosproject.net.resource.Resources;
 import org.onosproject.ui.topo.TopoConstants.CoreButtons;
 import org.onosproject.cli.AbstractShellCommand;
-
+import org.onlab.packet.Ip4Address;
+import org.onlab.packet.IpAddress;
 /**
  * PCE WEB topology overlay.
  */
@@ -49,11 +49,16 @@ public class PceWebTopovOverlay extends UiTopoOverlay {
     private static final String MY_TITLE = "Device details";
 
     public static final String AS_NUMBER = "asNumber";
-    public static final String DOMAIN_IDENTIFIER = "domainIdentifier";
+    public static final String LSR_ID = "lsrId";
     public static final String ABR_BIT = "abrBit";
     public static final String ASBR_BIT = "externalBit";
     public static final String TE_METRIC = "teCost";
-
+    public static final String ABR = "ABR";
+    public static final String ASBR = "ASBR";
+    public static final String ABR_ASBR = "ABR/ASBR";
+    public static final String INNER = "Inner";
+    public static final long IDENTIFIER_SET = 0x100000000L;
+    public static final long SET = 0xFFFFFFFFL;
     /**
      * Initialize the overlay ID.
      */
@@ -71,11 +76,8 @@ public class PceWebTopovOverlay extends UiTopoOverlay {
     public void modifyDeviceDetails(PropertyPanel pp, DeviceId deviceId) {
 
          pp.title(MY_TITLE);
-
          DeviceService deviceService = AbstractShellCommand.get(DeviceService.class);
-
          pp.removeAllProps();
-
          pp.removeButtons(CoreButtons.SHOW_PORT_VIEW)
                 .removeButtons(CoreButtons.SHOW_GROUP_VIEW)
                 .removeButtons(CoreButtons.SHOW_METER_VIEW);
@@ -85,10 +87,9 @@ public class PceWebTopovOverlay extends UiTopoOverlay {
             Device device = deviceService.getDevice(deviceId);
             Annotations annots = device.annotations();
 
-            String routerId = annots.value(AnnotationKeys.ROUTER_ID);
             String type = annots.value(AnnotationKeys.TYPE);
             String asNumber = annots.value(AS_NUMBER);
-            String domain = annots.value(DOMAIN_IDENTIFIER);
+            String lsrId = annots.value(LSR_ID);
             String abrStatus = annots.value(ABR_BIT);
             String asbrStatus = annots.value(ASBR_BIT);
 
@@ -96,24 +97,22 @@ public class PceWebTopovOverlay extends UiTopoOverlay {
                 pp.addProp("Type", type);
             }
 
-            if (routerId != null) {
-                pp.addProp("Router-ID", routerId);
-            }
-
             if (asNumber != null) {
                 pp.addProp("AS Number", asNumber);
             }
 
-            if (domain != null) {
-                pp.addProp("Domain ID", domain);
+            if (lsrId != null) {
+                pp.addProp("LSR ID", lsrId);
             }
 
-            if (abrStatus != null) {
-                pp.addProp("ABR Role", abrStatus);
-            }
-
-            if (asbrStatus != null) {
-                pp.addProp("ASBR Role", asbrStatus);
+            if (abrStatus.equals(true) && asbrStatus.equals(true)) {
+                pp.addProp("Position", ABR_ASBR);
+            } else if (abrStatus.equals(true)) {
+                pp.addProp("Position", ABR);
+            } else if (asbrStatus.equals(true)) {
+                pp.addProp("Position", ASBR);
+            } else {
+                pp.addProp("Position", INNER);
             }
         }
     }
@@ -122,30 +121,62 @@ public class PceWebTopovOverlay extends UiTopoOverlay {
     public Map<String, String> additionalLinkData(LinkEvent event) {
         Map<String, String> map = new HashMap<>();
         Link link = event.subject();
+        long srcPortNo;
+        long dstPortNo;
+        IpAddress ipDstAddress = null;
+        IpAddress ipSrcAddress = null;
+        String srcPort;
+        String dstPort;
+        String bandWidth;
 
-        map.put("Src port", link.src().port().toString());
-        map.put("Dst port", link.dst().port().toString());
+        srcPortNo = link.src().port().toLong();
+        if (((srcPortNo & IDENTIFIER_SET) == IDENTIFIER_SET)) {
+            srcPort = String.valueOf(srcPortNo);
+        } else {
+            ipSrcAddress = Ip4Address.valueOf((int) srcPortNo);
+            srcPort = ipSrcAddress.toString();
+        }
+
+        dstPortNo = link.dst().port().toLong();
+        if (((dstPortNo & IDENTIFIER_SET) == IDENTIFIER_SET)) {
+            dstPort = String.valueOf(dstPortNo);
+        } else {
+            ipDstAddress = Ip4Address.valueOf((int) dstPortNo);
+            dstPort = ipDstAddress.toString();
+        }
+
+        map.put("Src Address", srcPort);
+        map.put("Dst Address", dstPort);
         map.put("Te metric", link.annotations().value(TE_METRIC));
 
         ResourceService resService = AbstractShellCommand.get(ResourceService.class);
         DiscreteResource devResource = Resources.discrete(link.src().deviceId(), link.src().port()).resource();
         if (resService == null) {
-            log.warn("resource service does not exist ");
+            log.warn("resource service does not exist");
             return map;
         }
 
         if (devResource == null) {
-            log.warn("Device resources does not exist ");
+            log.warn("Device resources does not exist");
             return map;
         }
-        Set<Resource> resources = resService.getAvailableResources(devResource.id(), Bandwidth.class);
-        if (resources.isEmpty()) {
-            log.warn("Bandwidth resources does not exist ");
-            return map;
+        double regBandwidth = 0;
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            log.error("Exception occured while getting the bandwidth.");
+        }
+        Set<Resource> resources = resService.getRegisteredResources(devResource.id());
+        for (Resource res : resources) {
+            if (res instanceof ContinuousResource) {
+                regBandwidth = ((ContinuousResource) res).value();
+                break;
+            }
         }
 
-        if (resources.iterator().next() instanceof ContinuousResource) {
-            map.put("Bandwidth", ((ContinuousResource) resources.iterator().next()).toString());
+        if (regBandwidth != 0) {
+            bandWidth = String.valueOf(regBandwidth);
+            map.put("Bandwidth", bandWidth);
         }
 
         return map;
