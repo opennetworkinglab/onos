91a99126450efb2533fa0254ab117ca3654ac415
Pier Ventre <pierventre@hotmail.com>
2016-07-09 11:10:47 +0200

    Adds the control for Encapsulation Constraint.
    Change-Id: I98b09704d077d55ea0f65b15fb11da61e52851ac

--- core/api/src/main/java/org/onosproject/net/intent/IntentUtils.java
+++ core/api/src/main/java/org/onosproject/net/intent/IntentUtils.java
@@ -58,6 +58,7 @@ public final class IntentUtils {
 
             return Objects.equals(intent1.selector(), intent2.selector()) &&
                     Objects.equals(intent1.treatment(), intent2.treatment()) &&
+                    Objects.equals(intent1.constraints(), intent2.constraints()) &&
                     Objects.equals(intent1.ingressPoint(), intent2.ingressPoint()) &&
                     Objects.equals(intent1.egressPoints(), intent2.egressPoints());
         } else if (one instanceof MultiPointToSinglePointIntent) {
@@ -66,6 +67,7 @@ public final class IntentUtils {
 
             return Objects.equals(intent1.selector(), intent2.selector()) &&
                     Objects.equals(intent1.treatment(), intent2.treatment()) &&
+                    Objects.equals(intent1.constraints(), intent2.constraints()) &&
                     Objects.equals(intent1.ingressPoints(), intent2.ingressPoints()) &&
                     Objects.equals(intent1.egressPoint(), intent2.egressPoint());
         } else if (one instanceof PointToPointIntent) {
@@ -74,6 +76,7 @@ public final class IntentUtils {
 
             return Objects.equals(intent1.selector(), intent2.selector()) &&
                     Objects.equals(intent1.treatment(), intent2.treatment()) &&
+                    Objects.equals(intent1.constraints(), intent2.constraints()) &&
                     Objects.equals(intent1.ingressPoint(), intent2.ingressPoint()) &&
                     Objects.equals(intent1.egressPoint(), intent2.egressPoint());
         } else {
