ebc21174a7d145b8150b53674b509c51e92b374a
Thiago Santos <thiago@waltznetworks.com>
2016-08-05 17:58:56 -0300

    OpenFlowControllerImpl: always cleanup on deactivate
    Even if we don't have devices, the sockets/channels must be closed.
    
    This is particularly harmful when onos is restarted to form a cluster
    and the openflow channels are left open. On the next
    activation it will try to open ports that were already open and it
    fails
    
    Change-Id: I768b9db0b71ffba2805fabf631adcb2039af89e4

--- protocols/openflow/ctl/src/main/java/org/onosproject/openflow/controller/impl/OpenFlowControllerImpl.java
+++ protocols/openflow/ctl/src/main/java/org/onosproject/openflow/controller/impl/OpenFlowControllerImpl.java
@@ -174,9 +174,7 @@ public class OpenFlowControllerImpl implements OpenFlowController {
 
     @Deactivate
     public void deactivate() {
-        if (!connectedSwitches.isEmpty()) {
-            cleanup();
-        }
+        cleanup();
         cfgService.unregisterProperties(getClass(), false);
     }
 
