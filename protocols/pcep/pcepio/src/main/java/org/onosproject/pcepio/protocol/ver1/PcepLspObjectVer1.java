b7173afa28d102aa04b813cc12ddc2f4195d9cdf
Avantika-Huawei <avantika.s@huawei.com>
2016-08-09 11:42:11 +0530

    Making ERO non-mandatory (for end of LSP DB sync msg from cisco)
    Change-Id: I28e35520b5bd5737117d8c2b84f9effa3c46bfaf
    (cherry picked from commit fb630488ed0f6889eb0c6d38bbf7f08a6ea13ca4)

--- protocols/pcep/pcepio/src/main/java/org/onosproject/pcepio/protocol/ver1/PcepLspObjectVer1.java
+++ protocols/pcep/pcepio/src/main/java/org/onosproject/pcepio/protocol/ver1/PcepLspObjectVer1.java
@@ -340,7 +340,7 @@ public class PcepLspObjectVer1 implements PcepLspObject {
 
         while (MINIMUM_COMMON_HEADER_LENGTH <= cb.readableBytes()) {
 
-            PcepValueType tlv;
+            PcepValueType tlv = null;
             short hType = cb.readShort();
             short hLength = cb.readShort();
             int iValue = 0;
@@ -364,7 +364,10 @@ public class PcepLspObjectVer1 implements PcepLspObject {
                 tlv = StatefulLspDbVerTlv.read(cb);
                 break;
             default:
-                throw new PcepParseException("Received unsupported TLV type :" + hType);
+                // Skip the unknown TLV.
+                cb.skipBytes(hLength);
+                tlv = null;
+                log.info("Received unsupported TLV type :" + hType + " in LSP object.");
             }
             // Check for the padding
             int pad = hLength % 4;
@@ -375,7 +378,9 @@ public class PcepLspObjectVer1 implements PcepLspObject {
                 }
             }
 
-            llOutOptionalTlv.add(tlv);
+            if (tlv != null) {
+                llOutOptionalTlv.add(tlv);
+            }
         }
 
         if (0 < cb.readableBytes()) {
