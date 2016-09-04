317f329e56c1ccbcd28e4ffdb29424a8cbbd3ebf
Viswanath KSP <kspviswa.github@gmail.com>
2016-09-04 14:13:22 +0530

    [onos-5204] This commit contains fix for bug onos-5204 Addressed review comments Addressed review comments - take2
    Change-Id: I9eb1e19f70deffd33d57cdab6bb0346bd2ebbd98

--- web/gui/src/main/webapp/app/view/intent/intent.js
+++ web/gui/src/main/webapp/app/view/intent/intent.js
@@ -35,6 +35,8 @@
             $scope.briefTip = 'Switch to brief view';
             $scope.detailTip = 'Switch to detailed view';
             $scope.brief = true;
+            $scope.intentState = 'NA';
+            $scope.fired = false;
 
             function selCb($event, row) {
                 $log.debug('Got a click on:', row);
@@ -47,12 +49,24 @@
                     appName: name,
                     key: row.key
                 } : null;
+
+                $scope.intentState = row.state;
+            }
+
+            function respCb() {
+                if ($scope.fired) {
+                    if ($scope.changedData) {
+                        $scope.intentState = $scope.changedData.state;
+                    }
+                    $scope.fired = false;
+                }
             }
 
             tbs.buildTable({
                 scope: $scope,
                 tag: 'intent',
                 selCb: selCb,
+                respCb: respCb,
                 idKey: 'key'
             });
 
@@ -64,6 +78,10 @@
                 d && ns.navTo('topo', d);
             };
 
+            $scope.isIntentInstalled = function () {
+                return $scope.intentState === 'Installed';
+            };
+
             $scope.deactivateIntent = function () {
                 var content = ds.createDiv();
 
@@ -73,6 +91,7 @@
                 function dOk() {
                     var d = $scope.intentData;
                     d && tts.removeIntent(d);
+                    $scope.fired = true;
                 }
 
                 function dCancel() {
