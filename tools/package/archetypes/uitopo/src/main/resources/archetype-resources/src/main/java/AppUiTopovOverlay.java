c1050b260e6065b072b981a28be9c177902f71ab
Simon Hunt <simon@onlab.us>
2016-09-20 19:38:02 -0700

    Cleaning up Javadocs in core-ui classes. Added common constants to AbstractCellFormatter.
    Change-Id: I02081506c705e15dedb56e9895106b52e2c0aa7c

--- tools/package/archetypes/uitopo/src/main/resources/archetype-resources/src/main/java/AppUiTopovOverlay.java
+++ tools/package/archetypes/uitopo/src/main/resources/archetype-resources/src/main/java/AppUiTopovOverlay.java
@@ -23,7 +23,7 @@ import org.onosproject.ui.UiTopoOverlay;
 import org.onosproject.ui.topo.ButtonId;
 import org.onosproject.ui.topo.PropertyPanel;
 import org.onosproject.ui.topo.TopoConstants.CoreButtons;
-import org.onosproject.ui.topo.TopoConstants.Glyphs;
+import org.onosproject.ui.topo.GlyphConstants.CROWN;
 
 import static org.onosproject.ui.topo.TopoConstants.Properties.FLOWS;
 import static org.onosproject.ui.topo.TopoConstants.Properties.INTENTS;
@@ -56,7 +56,7 @@ public class AppUiTopovOverlay extends UiTopoOverlay {
     @Override
     public void modifySummary(PropertyPanel pp) {
         pp.title(MY_TITLE)
-                .typeId(Glyphs.CROWN)
+                .typeId(CROWN)
                 .removeProps(
                         TOPOLOGY_SSCS,
                         INTENTS,
