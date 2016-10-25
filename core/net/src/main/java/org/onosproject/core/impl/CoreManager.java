7fbb9b39407fe0bd0f7857dab4c8163048abca31
Brian O'Connor <bocon@onlab.us>
2016-10-24 22:30:47 -0700

    Tagging 1.7.1 (Hummingbird)

--- core/net/src/main/java/org/onosproject/core/impl/CoreManager.java
+++ core/net/src/main/java/org/onosproject/core/impl/CoreManager.java
@@ -64,7 +64,7 @@ public class CoreManager implements CoreService {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     private static final File VERSION_FILE = new File("../VERSION");
-    private static Version version = Version.version("1.7.1-SNAPSHOT");
+    private static Version version = Version.version("1.7.1");
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     protected ApplicationIdStore applicationIdStore;
