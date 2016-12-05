99f6f281c38b5b06ba4583000ecbd5bff3763ba5
Luca Prete <luca@onlab.us>
2016-12-05 15:33:36 -0800

    Correcting license typo in VPLS
    Change-Id: Id618607aeda453c9cca3df8606550ba590619a4e

--- apps/vpls/src/main/java/org/onosproject/vpls/cli/VplsCommandEnum.java
+++ apps/vpls/src/main/java/org/onosproject/vpls/cli/VplsCommandEnum.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-present Open Networking Laboratory
+ * Copyright 2015-present Open Networking Laboratory
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.onosproject.vpls.cli;
 
--- apps/vpls/src/test/java/org/onosproject/vpls/VplsNeighbourHandlerTest.java
+++ apps/vpls/src/test/java/org/onosproject/vpls/VplsNeighbourHandlerTest.java
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.onosproject.vpls;
 
 import com.google.common.collect.HashMultimap;
