1dd60121d8d51ce8bfa021750f8e276b944d1e9a
Priyanka B <priyanka.b@huawei.com>
2016-12-29 16:33:45 +0530

    [ONOS] Defect fix label downloading for SR-TE
    Change-Id: I8ddcd17af1a630f965f171e80096e69dae2e1850

--- protocols/pcep/ctl/src/main/java/org/onosproject/pcep/controller/impl/PceccSrTeBeHandler.java
+++ protocols/pcep/ctl/src/main/java/org/onosproject/pcep/controller/impl/PceccSrTeBeHandler.java
@@ -242,7 +242,7 @@ public final class PceccSrTeBeHandler {
                                         PcepLabelOp.ADD,
                                         false);
 
-                    pushGlobalNodeLabel(pcc, specificLabelId,
+                    pushGlobalNodeLabel(pcc, element.getValue(),
                                         IpAddress.valueOf(otherLsrId).getIp4Address().toInt(),
                                         PcepLabelOp.ADD,
                                         false);
