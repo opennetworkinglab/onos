e7006dca2a0c3fab7998a54a59628c20e6104c95
Andrea Campanella <andrea@onlab.us>
2017-02-15 16:04:09 -0800

    ONOS-6020 RolledBack and deprecated a method in NetconfDeviceInfo to not break backwards compatibility
    Change-Id: Ie029f6b1d3688d428130b76ed2e06c760d9a390a

--- protocols/netconf/api/src/main/java/org/onosproject/netconf/NetconfDeviceInfo.java
+++ protocols/netconf/api/src/main/java/org/onosproject/netconf/NetconfDeviceInfo.java
@@ -22,6 +22,7 @@ import org.onosproject.net.DeviceId;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Objects;
@@ -39,6 +40,8 @@ public class NetconfDeviceInfo {
     private IpAddress ipAddress;
     private int port;
     private char[] key;
+    //File keyFile @deprecated 1.9.0
+    private File keyFile;
     private DeviceId deviceId;
 
 
@@ -68,7 +71,12 @@ public class NetconfDeviceInfo {
      * @param password  the password for the device
      * @param ipAddress the ip address
      * @param port      the tcp port
-     * @param keyString the string containing the key.
+     * @param keyString the string containing a DSA or RSA private key
+     *                  of the user in OpenSSH key format
+     *                  <br>
+     *                  (Pre 1.9.0 behaviour: {@code keyString} can be file path
+     *                  to a file containing DSA or RSA private key of the user
+     *                  in OpenSSH key format)
      */
     public NetconfDeviceInfo(String name, String password, IpAddress ipAddress,
                              int port, String keyString) {
@@ -80,6 +88,7 @@ public class NetconfDeviceInfo {
         this.ipAddress = ipAddress;
         this.port = port;
         this.key = keyString.toCharArray();
+        this.keyFile = new File(keyString);
     }
 
     /**
@@ -121,10 +130,25 @@ public class NetconfDeviceInfo {
     /**
      * Exposes the key of the controller.
      *
-     * @return int port address
+     * @return {@code char[]} containing a DSA or RSA private key of the user
+     *         in OpenSSH key format
+     *         or null if device is not configured to use public key authentication
      */
     public char[] getKey() {
         return key;
+    }
+
+    /**
+     * Exposes the keyFile of the controller.
+     *
+     * @return File object pointing to a file containing a DSA or RSA
+     *         private key of the user in OpenSSH key format,
+     *         or null if device is not configured to use public key authentication
+     * @deprecated 1.9.0
+     */
+    @Deprecated
+    public File getKeyFile() {
+        return keyFile;
     }
 
     /**
@@ -133,6 +157,7 @@ public class NetconfDeviceInfo {
      *
      * @return String device info
      */
+    @Override
     public String toString() {
         return "netconf:" + name + "@" + ipAddress + ":" + port;
     }
