3ff327af1f0bc61d0bc252f2a0f75f2beac9b446
Jian Li <pyguni@gmail.com>
2017-03-14 17:02:19 +0900

    [ONOS-6142] Add skeleton code for LISP extension addresses
    Change-Id: I692c09c28d561a13df5de6db79b979a2e820ff09

---
+++ drivers/lisp/src/main/java/org/onosproject/drivers/lisp/extensions/LispAppDataAddress.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2017-present Open Networking Laboratory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onosproject.drivers.lisp.extensions;
+
+import org.onosproject.mapping.addresses.ExtensionMappingAddress;
+import org.onosproject.mapping.addresses.ExtensionMappingAddressType;
+import org.onosproject.net.flow.AbstractExtension;
+
+/**
+ * Implementation of LISP application data address.
+ */
+public class LispAppDataAddress extends AbstractExtension
+                                            implements ExtensionMappingAddress {
+    @Override
+    public ExtensionMappingAddressType type() {
+        return null;
+    }
+
+    @Override
+    public byte[] serialize() {
+        return new byte[0];
+    }
+
+    @Override
+    public void deserialize(byte[] data) {
+
+    }
+}
