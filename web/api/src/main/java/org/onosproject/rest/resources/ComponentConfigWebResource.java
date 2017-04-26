c86d95f969864664beb3948bc5ff2c455e372393
sivachidambaram subramanian <sisubram@partner.ciena.com>
2017-04-26 14:59:02 +0530

    [ONOS-6354] CFG values and applied values are mismatching while trying to set a property with an invalid value
    Change-Id: Id2e343c61419441d0b9554df3291b35829e850ab

--- web/api/src/main/java/org/onosproject/rest/resources/ComponentConfigWebResource.java
+++ web/api/src/main/java/org/onosproject/rest/resources/ComponentConfigWebResource.java
@@ -15,6 +15,7 @@
  */
 package org.onosproject.rest.resources;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.onosproject.cfg.ComponentConfigService;
 import org.onosproject.cfg.ConfigProperty;
@@ -33,6 +34,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Set;
 
 import static org.onlab.util.Tools.nullIsNotFound;
@@ -42,6 +45,7 @@ import static org.onlab.util.Tools.nullIsNotFound;
  */
 @Path("configuration")
 public class ComponentConfigWebResource extends AbstractWebResource {
+    private static final int  MULTI_STATUS_RESPONE = 207;
 
     /**
      * Gets all component configurations.
@@ -102,14 +106,34 @@ public class ComponentConfigWebResource extends AbstractWebResource {
                                InputStream request) throws IOException {
         ComponentConfigService service = get(ComponentConfigService.class);
         ObjectNode props = (ObjectNode) mapper().readTree(request);
+        List<String> errorMsgs = new ArrayList<String>();
         if (preset) {
-            props.fieldNames().forEachRemaining(k ->
-                    service.preSetProperty(component, k, props.path(k).asText()));
+                props.fieldNames().forEachRemaining(k -> {
+                    try {
+                        service.preSetProperty(component, k, props.path(k).asText());
+                    } catch (IllegalArgumentException e) {
+                        errorMsgs.add(e.getMessage());
+                    }
+                });
         } else {
-            props.fieldNames().forEachRemaining(k ->
-                    service.setProperty(component, k, props.path(k).asText()));
+                props.fieldNames().forEachRemaining(k -> {
+                    try {
+                        service.setProperty(component, k, props.path(k).asText());
+                    } catch (IllegalArgumentException e) {
+                        errorMsgs.add(e.getMessage());
+                    }
+                });
+        }
+        if (!errorMsgs.isEmpty()) {
+            return Response.status(MULTI_STATUS_RESPONE).entity(produceErrorJson(errorMsgs)).build();
         }
         return Response.ok().build();
+    }
+
+    private ObjectNode produceErrorJson(List<String> errorMsgs) {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode result = mapper.createObjectNode().put("code", 207).putPOJO("message", errorMsgs);
+        return result;
     }
 
     /**
