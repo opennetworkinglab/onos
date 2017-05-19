16efe17b4fc20cc618daaed1eb0c17fd0529dde9
Ray Milkey <ray@onlab.us>
2017-05-19 15:03:13 -0700

    Remove deprecated flag on a method that has no replacement
    Change-Id: Iece715e3c6985f0df77c23a3ef6f3b6dab9cb2db

--- core/api/src/main/java/org/onosproject/net/Link.java
+++ core/api/src/main/java/org/onosproject/net/Link.java
@@ -110,9 +110,7 @@ public interface Link extends Annotated, Provided, Projectable, NetworkResource 
      * Indicates if the link is to be considered durable.
      *
      * @return true if the link is durable
-     * @deprecated in Falcon Release - replaced by isConfigured()
      */
-    @Deprecated
     boolean isDurable();
 
     /**
