f1197235a2bccd20d8fa551f7d4b3370eb9be3dd
Yoonseon Han <yoonseon@onlab.us>
2017-05-30 15:05:20 -0700

    [ONOS-6542] (vNet) Distributed Flow Rule store
    An implementation of distributed flow rule store
    for virtual networks
    
    Change-Id: Ie9d94d42177c84aaf6b89b591a5c6a3722c95401

--- incubator/store/src/main/java/org/onosproject/incubator/store/virtual/impl/primitives/package-info.java
+++ incubator/store/src/main/java/org/onosproject/incubator/store/virtual/impl/primitives/package-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015-present Open Networking Foundation
+ * Copyright 2017-present Open Networking Foundation
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,6 +15,6 @@
  */
 
 /**
- * Implementation of virtual network stores.
+ * Implementation of distributed virtual network store primitives.
  */
-package org.onosproject.incubator.store.virtual.impl;
+package org.onosproject.incubator.store.virtual.impl.primitives;
