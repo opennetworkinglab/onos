8daf45c6f54c21a80a59b3e898e2d614c4373178
Charles Chan <rascov@gmail.com>
2017-06-22 14:27:17 -0700

    Lower log level
    Change-Id: I1801338d75e063399035cafc541deb80d775ddda

--- apps/segmentrouting/src/main/java/org/onosproject/segmentrouting/RoutingRulePopulator.java
+++ apps/segmentrouting/src/main/java/org/onosproject/segmentrouting/RoutingRulePopulator.java
@@ -656,7 +656,7 @@ public class RoutingRulePopulator {
                 errorPorts++;
             }
         }
-        log.info("Filtering on dev:{}, disabledPorts:{}, errorPorts:{}, filteredPorts:{}",
+        log.debug("Filtering on dev:{}, disabledPorts:{}, errorPorts:{}, filteredPorts:{}",
                   deviceId, disabledPorts, errorPorts, filteredPorts);
         return srManager.defaultRoutingHandler.new PortFilterInfo(disabledPorts,
                                                        errorPorts, filteredPorts);
