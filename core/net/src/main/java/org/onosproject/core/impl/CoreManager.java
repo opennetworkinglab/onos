7e28519e5f4d4c984ce45414cb08082efa53e203
Ray Milkey <ray@opennetworking.org>
2017-07-20 15:45:28 -0700

    Tagging 1.11.0-b2

--- core/net/src/main/java/org/onosproject/core/impl/CoreManager.java
+++ core/net/src/main/java/org/onosproject/core/impl/CoreManager.java
@@ -65,7 +65,7 @@ public class CoreManager implements CoreService {
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     private static final File VERSION_FILE = new File("../VERSION");
-    private static Version version = Version.version("1.11.0-SNAPSHOT");
+    private static Version version = Version.version("1.11.0-b2");
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY_UNARY)
     protected ApplicationIdStore applicationIdStore;
