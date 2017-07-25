4e55301dbc7cd35ab757e40f3ee087c13bc04980
Charles Chan <rascov@gmail.com>
2017-07-24 21:13:01 -0700

    Show host provider in CLI
    Change-Id: I2094197c3a684c67daa3801ac31680406abcb4c8

--- cli/src/main/java/org/onosproject/cli/net/HostsListCommand.java
+++ cli/src/main/java/org/onosproject/cli/net/HostsListCommand.java
@@ -38,7 +38,7 @@ import static com.google.common.collect.Lists.newArrayList;
 public class HostsListCommand extends AbstractShellCommand {
 
     private static final String FMT =
-            "id=%s, mac=%s, locations=%s, vlan=%s, ip(s)=%s%s, configured=%s";
+            "id=%s, mac=%s, locations=%s, vlan=%s, ip(s)=%s%s, provider=%s:%s, configured=%s";
 
     private static final String FMT_SHORT =
             "id=%s, mac=%s, locations=%s, vlan=%s, ip(s)=%s";
@@ -94,6 +94,7 @@ public class HostsListCommand extends AbstractShellCommand {
             print(FMT, host.id(), host.mac(),
                   host.locations(),
                   host.vlan(), host.ipAddresses(), annotations(host.annotations()),
+                  host.providerId().scheme(), host.providerId().id(),
                   host.configured());
         }
     }
