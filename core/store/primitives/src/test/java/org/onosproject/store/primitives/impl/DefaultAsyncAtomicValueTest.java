ffe02345e87154bff7feecf12131f0c552d03a72
Sbhat35 <sachit.s.bhat@vanderbilt.edu>
2017-08-09 16:31:17 -0700

    -Onos store refactoring -Added unit tests for TranscodingAsyncConsistMap, AsyncDistributedSet
    Change-Id: I744aef36a7086a941f7a3809ab632dbf7ec2c89b

--- core/store/primitives/src/test/java/org/onosproject/store/primitives/impl/DefaultAsyncAtomicValueTest.java
+++ core/store/primitives/src/test/java/org/onosproject/store/primitives/impl/DefaultAsyncAtomicValueTest.java
@@ -19,26 +19,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.onlab.util.Tools;
-import org.onosproject.store.primitives.MapUpdate;
-import org.onosproject.store.primitives.TransactionId;
 import org.onosproject.store.serializers.KryoNamespaces;
 import org.onosproject.store.service.AsyncConsistentMap;
-import org.onosproject.store.service.MapEventListener;
 import org.onosproject.store.service.Serializer;
-import org.onosproject.store.service.TransactionLog;
-import org.onosproject.store.service.Version;
-import org.onosproject.store.service.Versioned;
 import org.onosproject.utils.MeteringAgent;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.Executor;
-import java.util.function.BiFunction;
-import java.util.function.Predicate;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertNull;
@@ -46,15 +30,14 @@ import static org.junit.Assert.assertThat;
 
 
 public class DefaultAsyncAtomicValueTest {
-    DefaultAsyncAtomicValue defaultAsyncAtomicValue;
-    DefaultAsyncAtomicValue defaultAsyncAtomicValue1;
-
+    DefaultAsyncAtomicValue<String> defaultAsyncAtomicValue;
 
     private AsyncConsistentMap<String, byte[]> asyncMap;
-    private Map<String, Versioned<byte[]>> map;
 
     private Serializer serializer;
     private MeteringAgent meteringAgent;
+
+    private static final String MAPNAME = "map1";
 
 
     private static final String NAME = "atomicValue";
@@ -71,175 +54,11 @@ public class DefaultAsyncAtomicValueTest {
 
     @Before
     public void setUp() throws Exception {
-        map = new HashMap<>();
-        asyncMap = new AsyncConsistentMap<String, byte[]>() {
-            @Override
-            public CompletableFuture<Integer> size() {
-                return CompletableFuture.completedFuture(map.size());
-            }
-
-            @Override
-            public CompletableFuture<Boolean> containsKey(String key) {
-                return CompletableFuture.completedFuture(map.containsKey(key));
-            }
-
-            @Override
-            public CompletableFuture<Boolean> containsValue(byte[] value) {
-                return CompletableFuture.completedFuture(map.containsValue(value));
-            }
-
-            @Override
-            public CompletableFuture<Versioned<byte[]>> get(String key) {
-                return CompletableFuture.completedFuture(map.get(key));
-            }
-
-            @Override
-            public CompletableFuture<Versioned<byte[]>> getOrDefault(String key, byte[] defaultValue) {
-                return CompletableFuture.completedFuture(map.getOrDefault(key, new Versioned<byte[]>(defaultValue,
-                        VERSION1)));
-            }
-
-            @Override
-            public CompletableFuture<Versioned<byte[]>> computeIf(String key, Predicate<? super byte[]> condition,
-                                                                  BiFunction<? super String, ? super byte[],
-                                                                          ? extends byte[]> remappingFunction) {
-                return null;
-            }
-
-            @Override
-            public CompletableFuture<Versioned<byte[]>> put(String key, byte[] value) {
-                return CompletableFuture.completedFuture(map.put(key, new Versioned<byte[]>(value, VERSION1)));
-            }
-
-
-            @Override
-            public CompletableFuture<Versioned<byte[]>> putAndGet(String key, byte[] value) {
-                return null;
-            }
-
-            @Override
-            public CompletableFuture<Versioned<byte[]>> remove(String key) {
-                return CompletableFuture.completedFuture(map.remove(key));
-            }
-
-            @Override
-            public CompletableFuture<Void> clear() {
-                return null;
-            }
-
-            @Override
-            public CompletableFuture<Set<String>> keySet() {
-                return null;
-            }
-
-            @Override
-            public CompletableFuture<Collection<Versioned<byte[]>>> values() {
-                return CompletableFuture.completedFuture(map.values());
-            }
-
-            @Override
-            public CompletableFuture<Set<Map.Entry<String, Versioned<byte[]>>>> entrySet() {
-                return CompletableFuture.completedFuture(map.entrySet());
-            }
-
-            @Override
-            public CompletableFuture<Versioned<byte[]>> putIfAbsent(String key, byte[] value) {
-                return CompletableFuture.completedFuture(map.putIfAbsent(key, new Versioned<byte[]>(value, 2)));
-            }
-
-            @Override
-            public CompletableFuture<Boolean> remove(String key, byte[] value) {
-                return CompletableFuture.completedFuture(map.remove(key, value));
-            }
-
-            @Override
-            public CompletableFuture<Boolean> remove(String key, long version) {
-                Versioned versioned = map.get(key);
-                if (versioned.version() == version) {
-                    map.remove(key);
-                    return CompletableFuture.completedFuture(true);
-                }
-                return CompletableFuture.completedFuture(false);
-            }
-
-            @Override
-            public CompletableFuture<Versioned<byte[]>> replace(String key, byte[] value) {
-                return CompletableFuture.completedFuture(map.replace(key, new Versioned<byte[]>(value, VERSION1)));
-            }
-
-            @Override
-            public CompletableFuture<Boolean> replace(String key, byte[] oldValue, byte[] newValue) {
-                Versioned<byte[]> currentValue = map.get(key);
-                if (currentValue == null) {
-                    return CompletableFuture.completedFuture(false);
-                }
-
-                if (Arrays.equals(currentValue.value(), oldValue)) {
-                    map.put(key, new Versioned<>(newValue, VERSION1));
-                    return CompletableFuture.completedFuture(true);
-                }
-                return CompletableFuture.completedFuture(false);
-            }
-
-            @Override
-            public CompletableFuture<Boolean> replace(String key, long oldVersion, byte[] newValue) {
-                Versioned versioned = map.get(key);
-                if (versioned != null && versioned.version() == oldVersion) {
-                    map.put(key, new Versioned<byte[]>(newValue, VERSION1));
-                    return CompletableFuture.completedFuture(true);
-                }
-                return CompletableFuture.completedFuture(false);
-            }
-
-            @Override
-            public CompletableFuture<Void> addListener(MapEventListener<String, byte[]> listener, Executor executor) {
-                return null;
-            }
-
-            @Override
-            public CompletableFuture<Void> removeListener(MapEventListener<String, byte[]> listener) {
-                return null;
-            }
-
-            @Override
-            public String name() {
-                return null;
-            }
-
-            @Override
-            public CompletableFuture<Version> begin(TransactionId transactionId) {
-                return null;
-            }
-
-            @Override
-            public CompletableFuture<Boolean> prepare(TransactionLog<MapUpdate<String, byte[]>> transactionLog) {
-                return null;
-            }
-
-            @Override
-            public CompletableFuture<Boolean> prepareAndCommit(TransactionLog<MapUpdate<String,
-                    byte[]>> transactionLog) {
-                return null;
-            }
-
-            @Override
-            public CompletableFuture<Void> commit(TransactionId transactionId) {
-                return null;
-            }
-
-            @Override
-            public CompletableFuture<Void> rollback(TransactionId transactionId) {
-                return null;
-            }
-        };
-
+        asyncMap = new AsyncConsistentMapMock<>();
         serializer = Serializer.using(KryoNamespaces.BASIC);
         meteringAgent = new MeteringAgent(NAME, "*", false);
-        defaultAsyncAtomicValue = new DefaultAsyncAtomicValue(NAME, serializer,
+        defaultAsyncAtomicValue = new DefaultAsyncAtomicValue(MAPNAME, serializer,
                 asyncMap, meteringAgent);
-        defaultAsyncAtomicValue1 = new DefaultAsyncAtomicValue(NAME1, serializer,
-                asyncMap, meteringAgent);
-
     }
 
     @After
@@ -295,26 +114,25 @@ public class DefaultAsyncAtomicValueTest {
         defaultAsyncAtomicValue = new DefaultAsyncAtomicValue(NAME, serializer,
                 asyncMap, meteringAgent);
         assertThat(defaultAsyncAtomicValue.name(), is(NAME));
-        defaultAsyncAtomicValue.set(null);
+        defaultAsyncAtomicValue.set(null).join();
         assertNull(defaultAsyncAtomicValue.get().join());
 
-        defaultAsyncAtomicValue.set(INTNAME);
-        assertThat(defaultAsyncAtomicValue.get().join(), is(INTNAME));
+        defaultAsyncAtomicValue.set(NAME).join();
+        assertThat(defaultAsyncAtomicValue.get().join(), is(NAME));
 
-        defaultAsyncAtomicValue.set(value1);
-        assertThat(defaultAsyncAtomicValue.get().join(), is(value1));
+        defaultAsyncAtomicValue.set(NAME1).join();
+        assertThat(defaultAsyncAtomicValue.get().join(), is(NAME1));
+        defaultAsyncAtomicValue.compareAndSet(NAME1, NAME).join();
+        assertThat(defaultAsyncAtomicValue.get().join(), is(NAME));
 
-        defaultAsyncAtomicValue.compareAndSet(value1, value3).join();
-        assertThat(defaultAsyncAtomicValue.get().join(), is(value3));
 
-        assertThat(defaultAsyncAtomicValue.compareAndSet(value3, value1).join(),
-                is(true));
-        assertThat(defaultAsyncAtomicValue.get().join(), is(value1));
-
-        defaultAsyncAtomicValue.getAndSet(null);
+        defaultAsyncAtomicValue.getAndSet(null).join();
         assertNull(defaultAsyncAtomicValue.get().join());
 
-        defaultAsyncAtomicValue.getAndSet(value3);
-        assertThat(defaultAsyncAtomicValue.get().join(), is(value3));
+        defaultAsyncAtomicValue.set(NAME1).join();
+        assertThat(defaultAsyncAtomicValue.getAndSet(NAME).join(), is(NAME1));
+        assertThat(defaultAsyncAtomicValue.getAndSet("new").join(), is(NAME));
+
+        assertThat(defaultAsyncAtomicValue.get().join(), is("new"));
     }
 }
