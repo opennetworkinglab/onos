f14d9ef158cd8ede9a80f736d3ad225658c22df0
Saurav Das <sauravdas@alumni.stanford.edu>
2017-12-05 15:00:23 -0800

    Fix for empty flooding groups due to bucket add/remove commands being reordered, and triggered by port up/down events very close to each other.
    Change-Id: Ibbd7cc6eb6df35396175619d95439acd6da904b8

--- apps/segmentrouting/src/main/java/org/onosproject/segmentrouting/SegmentRoutingManager.java
+++ apps/segmentrouting/src/main/java/org/onosproject/segmentrouting/SegmentRoutingManager.java
@@ -1353,7 +1353,7 @@ public class SegmentRoutingManager implements SegmentRoutingService {
                 log.warn(e.getMessage() + " Aborting processDeviceAdded.");
                 return;
             }
-            log.debug("updating groupHandlerMap with new config for device: {}",
+            log.debug("updating groupHandlerMap with new grpHdlr for device: {}",
                     deviceId);
             groupHandlerMap.put(deviceId, groupHandler);
         }
