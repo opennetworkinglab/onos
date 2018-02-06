5767d599505cf92d9efe0a007f05ba8a65ac6e81
Thomas Vachuska <tom@opennetworking.org>
2018-02-05 17:18:06 -0800

    Updating tower topology for the tutorial.
    Change-Id: I5310d0fad33fe2dfbd4135fd350696c4d5a36458

--- tools/test/topos/tower.py
+++ tools/test/topos/tower.py
@@ -1,55 +1,42 @@
 #!/usr/bin/env python
 
+"""
+"""
 from mininet.topo import Topo
-from mininet.cli import CLI
-from mininet.net import Mininet
-from mininet.node import RemoteController, OVSKernelSwitch
-from mininet.log import setLogLevel
 
+class Tower( Topo ):
+    "Internet Topology Zoo Specimen."
 
-class TowerTopo( Topo ):
-    """Create a tower topology"""
+    def addSwitch( self, name, **opts ):
+        kwargs = { 'protocols' : 'OpenFlow13' }
+        kwargs.update( opts )
+        return super(Tower, self).addSwitch( name, **kwargs )
 
-    def build( self, k=4, h=6 ):
+    def __init__( self ):
+        "Create a topology."
+
+        # Initialize Topology
+        Topo.__init__( self )
+
         spines = []
-        leaves = []
-        hosts = []
 
         # Create the two spine switches
-        spines.append(self.addSwitch('s1'))
-        spines.append(self.addSwitch('s2'))
+        spines.append(self.addSwitch( 's1' ))
+        spines.append(self.addSwitch( 's2' ))
 
-        # Create two links between the spine switches
-        self.addLink(spines[0], spines[1])
-        #TODO add second link between spines when multi-link topos are supported
-        #self.addLink(spines[0], spines[1])
-        
         # Now create the leaf switches, their hosts and connect them together
-        i = 1
-        c = 0
-        while i <= k:
-            leaves.append(self.addSwitch('s1%d' % i))
+        for i in range(4):
+            sn = i + 1
+            leaf = self.addSwitch( 's1%d' % sn )
             for spine in spines:
-                self.addLink(leaves[i-1], spine)
+                self.addLink(leaf, spine)
 
-            j = 1
-            while j <= h:
-                hosts.append(self.addHost('h%d%d' % (i, j)))
-                self.addLink(hosts[c], leaves[i-1])
-                j+=1
-                c+=1
+            for j in range(5):
+                host = self.addHost( 'h%d%d' % (sn, j + 1) )
+                self.addLink( host, leaf )
 
-            i+=1
-
-topos = { 'tower': TowerTopo }
-
-def run():
-    topo = TowerTopo()
-    net = Mininet( topo=topo, controller=RemoteController, autoSetMacs=True )
-    net.start()
-    CLI( net )
-    net.stop()
+topos = { 'tower': ( lambda: Tower() ) }
 
 if __name__ == '__main__':
-    setLogLevel( 'info' )
-    run()
+    from onosnet import run
+    run( Tower() )
