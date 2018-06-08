61b4c34e216fdb3357bcf0ff1efaa00693b5960d
Ray Milkey <ray@opennetworking.org>
2018-06-08 09:36:57 -0700

    Fix maven swagger build of Cisco rest driver
    Change-Id: Ieed56d66339e3094a95c9a684f897d56e8be4478

--- drivers/cisco/rest/src/main/java/org/onosproject/drivers/cisco/rest/ControllerConfigCiscoImpl.java
+++ drivers/cisco/rest/src/main/java/org/onosproject/drivers/cisco/rest/ControllerConfigCiscoImpl.java
@@ -18,7 +18,7 @@ package org.onosproject.drivers.cisco.rest;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
-import java.io.IOException;;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
