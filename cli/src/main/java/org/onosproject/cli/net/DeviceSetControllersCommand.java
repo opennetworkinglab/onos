264e9307df2567904d2b8efc0ae7df6e177081c3
Andrea Campanella <andrea@opennetworking.org>
2018-06-26 11:16:51 +0200

    [OS] Fix for XXE in netconf drivers xml utils
    Change-Id: Ie38901decb59712c7cf6c717f42bbb746c1e1921

--- cli/src/main/java/org/onosproject/cli/net/DeviceSetControllersCommand.java
+++ cli/src/main/java/org/onosproject/cli/net/DeviceSetControllersCommand.java
@@ -54,8 +54,17 @@ public class DeviceSetControllersCommand extends AbstractShellCommand {
     @Override
     protected void execute() {
 
+        if (controllersListStrings == null) {
+            print("No controller are given, skipping.");
+            return;
+        }
         Arrays.asList(controllersListStrings).forEach(
-                cInfoString -> newControllers.add(parseCInfoString(cInfoString)));
+                cInfoString -> {
+                    ControllerInfo controllerInfo = parseCInfoString(cInfoString);
+                    if (controllerInfo != null) {
+                        newControllers.add(controllerInfo);
+                    }
+                });
         DriverService service = get(DriverService.class);
         deviceId = DeviceId.deviceId(uri);
         DriverHandler h = service.createHandler(deviceId);
@@ -88,15 +97,24 @@ public class DeviceSetControllersCommand extends AbstractShellCommand {
                 return null;
             }
 
-            String[] data = config[0].split(":");
-            String type = data[0];
-            IpAddress ip = IpAddress.valueOf(data[1]);
-            int port = Integer.parseInt(data[2]);
-
-            return new ControllerInfo(ip, port, type, annotation);
+            return getControllerInfo(annotation, config[0]);
         } else {
-            print(config[0]);
-            return new ControllerInfo(config[0]);
+            return getControllerInfo(null, config[0]);
         }
+    }
+
+    private ControllerInfo getControllerInfo(Annotations annotation, String s) {
+        String[] data = s.split(":");
+        if (data.length != 3) {
+            print("Wrong format of the controller %s, should be in the format <protocol>:<ip>:<port>", s);
+            return null;
+        }
+        String type = data[0];
+        IpAddress ip = IpAddress.valueOf(data[1]);
+        int port = Integer.parseInt(data[2]);
+        if (annotation != null) {
+            return new ControllerInfo(ip, port, type, annotation);
+        }
+        return new ControllerInfo(ip, port, type);
     }
 }
