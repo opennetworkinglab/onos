1d6b7dba37fef6a366cca0baea7742b800f96601
jayakumarthazhath <mike.tiwari@gmail.com>
2018-09-25 15:56:51 -0400

    Get the matching vlan entry from DHCP record if more then one entry is present
    Change-Id: I73a00c1887e4e6601ab87c31b954c936c5d923be

--- apps/dhcprelay/src/main/java/org/onosproject/dhcprelay/Dhcp6HandlerImpl.java
+++ apps/dhcprelay/src/main/java/org/onosproject/dhcprelay/Dhcp6HandlerImpl.java
@@ -1212,7 +1212,8 @@ public class Dhcp6HandlerImpl implements DhcpHandler, HostProvider {
         DhcpServerInfo foundServerInfo = findServerInfoFromServer(directConnFlag, inPort);
 
         if (foundServerInfo == null) {
-            log.warn("Cannot find server info");
+            log.warn("Cannot find server info for {} server, inPort {}",
+                      directConnFlag ? "direct" : "indirect", inPort);
             dhcpRelayCountersStore.incrementCounter(gCount, DhcpRelayCounters.NO_SERVER_INFO);
             return null;
         } else {
@@ -1847,9 +1848,6 @@ public class Dhcp6HandlerImpl implements DhcpHandler, HostProvider {
                 log.debug("ServerInfo found for Rcv port {} Server Connect Point {} for {}",
                         inPort, serverInfo.getDhcpServerConnectPoint(), directConnFlag ? "direct" : "indirect");
                 break;
-            } else {
-                log.warn("Rcv port {} not the same as Server Connect Point {} for {}",
-                        inPort, serverInfo.getDhcpServerConnectPoint(), directConnFlag ? "direct" : "indirect");
             }
         }
         return foundServerInfo;
