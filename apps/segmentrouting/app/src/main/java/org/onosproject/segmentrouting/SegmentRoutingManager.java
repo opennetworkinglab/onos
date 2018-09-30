2b50bc03555d16150f97a0501b00bcc4bba53632
jayakumarthazhath <jayakumar.thazhath@infosys.com>
2018-10-01 00:51:54 +0530

    Prevent XConnect loop
    Change-Id: I65c52342840ebef944a65b8c6f65a33448da59cf

--- apps/segmentrouting/app/src/main/java/org/onosproject/segmentrouting/SegmentRoutingManager.java
+++ apps/segmentrouting/app/src/main/java/org/onosproject/segmentrouting/SegmentRoutingManager.java
@@ -997,6 +997,11 @@ public class SegmentRoutingManager implements SegmentRoutingService {
     }
 
     @Override
+    public boolean shouldProgram(DeviceId deviceId) {
+        return defaultRoutingHandler.shouldProgram(deviceId);
+    }
+
+    @Override
     public ApplicationId appId() {
         return appId;
     }
