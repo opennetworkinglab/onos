f8b2aab4345b3a4285ba52bfa8c66df1c1ebd3cc
Carmelo Cascone <carmelo@opennetworking.org>
2018-11-29 13:20:20 -0800

    Sometimes we need to create multicast groups without replicas
    Change-Id: I29dd07115efece0cc2d7d998317cfc3bfd7a1569
    (cherry picked from commit 00552ba153a61e2d904e6fcf36d25330630bb8e1)

--- core/api/src/main/java/org/onosproject/net/pi/runtime/PiMulticastGroupEntry.java
+++ core/api/src/main/java/org/onosproject/net/pi/runtime/PiMulticastGroupEntry.java
@@ -24,7 +24,6 @@ import com.google.common.collect.ImmutableSet;
 import java.util.Collection;
 import java.util.Set;
 
-import static com.google.common.base.Preconditions.checkArgument;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
@@ -161,7 +160,6 @@ public final class PiMulticastGroupEntry implements PiPreEntry {
         public PiMulticastGroupEntry build() {
             checkNotNull(groupId, "Multicast group ID must be set");
             final ImmutableSet<PiPreReplica> replicas = replicaSetBuilder.build();
-            checkArgument(!replicas.isEmpty(), "At least one replica must be defined");
             return new PiMulticastGroupEntry(groupId, replicas);
         }
     }
