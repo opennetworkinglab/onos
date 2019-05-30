b73edd97d203100346d107d46b3c10f6e58c80c5
Daniel Park <dan.mcpark84@gmail.com>
2019-05-30 10:43:32 +0900

    Fixes openstack purge rules CLI operate based hostnames
    Change-Id: I94122dcb83a357591cb2d4eea56d3d665e336c5d

--- apps/openstacknetworking/app/src/main/java/org/onosproject/openstacknetworking/cli/OpenstackPurgeRulesCommand.java
+++ apps/openstacknetworking/app/src/main/java/org/onosproject/openstacknetworking/cli/OpenstackPurgeRulesCommand.java
@@ -15,16 +15,22 @@
  */
 package org.onosproject.openstacknetworking.cli;
 
+import org.apache.karaf.shell.commands.Argument;
 import org.apache.karaf.shell.commands.Command;
 import org.onosproject.cli.AbstractShellCommand;
 import org.onosproject.core.ApplicationId;
 import org.onosproject.core.CoreService;
+import org.onosproject.net.DeviceId;
+import org.onosproject.net.flow.FlowEntry;
 import org.onosproject.net.flow.FlowRuleService;
 import org.onosproject.openstacknetworking.api.Constants;
+import org.onosproject.openstacknode.api.OpenstackNodeService;
+
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static java.lang.Thread.sleep;
 import static java.util.stream.StreamSupport.stream;
-import static org.onosproject.cli.AbstractShellCommand.get;
 
 /**
  * Purges all existing network states.
@@ -36,53 +42,91 @@ public class OpenstackPurgeRulesCommand extends AbstractShellCommand {
     private static final long TIMEOUT_MS = 10000; // we wait 10s
     private static final long SLEEP_MS = 2000; // we wait 2s for init each node
 
+    @Argument(name = "hostname", description = "Hostname",
+            required = true, multiValued = true)
+    private String[] hostnames = null;
+
     @Override
     protected void execute() {
         FlowRuleService flowRuleService = get(FlowRuleService.class);
         CoreService coreService = get(CoreService.class);
+        OpenstackNodeService nodeService = get(OpenstackNodeService.class);
         ApplicationId appId = coreService.getAppId(Constants.OPENSTACK_NETWORKING_APP_ID);
 
         if (appId == null) {
-            error("Failed to purge OpenStack networking flow rules.");
+            error("Failed to purge OpenStack networking flow rules because of null app ID");
             return;
         }
 
-        flowRuleService.removeFlowRulesById(appId);
-        print("Successfully purged flow rules installed by OpenStack networking app.");
+        if (hostnames == null) {
+            error("Failed to purge OpenStack networking flow rules because of null hostnames");
+            return;
+        }
 
-        boolean result = true;
-        long timeoutExpiredMs = System.currentTimeMillis() + TIMEOUT_MS;
-
-        // we make sure all flow rules are removed from the store
-        while (stream(flowRuleService.getFlowEntriesById(appId)
-                                     .spliterator(), false).count() > 0) {
-
-            long  waitMs = timeoutExpiredMs - System.currentTimeMillis();
-
-            try {
-                sleep(SLEEP_MS);
-            } catch (InterruptedException e) {
-                log.error("Exception caused during rule purging...");
+        for (String hostname : hostnames) {
+            if (nodeService.node(hostname) == null) {
+                error("Failed to purge OpenStack networking flow rules for %s because of null openstack node",
+                        hostname);
+                continue;
             }
 
-            if (stream(flowRuleService.getFlowEntriesById(appId)
-                                      .spliterator(), false).count() == 0) {
-                break;
+            DeviceId deviceId = nodeService.node(hostname).intgBridge();
+            if (deviceId == null) {
+                error("Failed to purge OpenStack networking flow rules because of null device ID");
+                return;
+            }
+
+            removeFlowRulesByDeviceId(appId, flowRuleService, deviceId);
+            print("Successfully purged flow rules installed by" +
+                    " OpenStack networking app on host %s.", hostname);
+
+            boolean result = true;
+            long timeoutExpiredMs = System.currentTimeMillis() + TIMEOUT_MS;
+
+            // we make sure all flow rules are removed from the store
+            while (getFlowEntriesByDeviceId(appId, flowRuleService, deviceId).isEmpty()) {
+
+                long  waitMs = timeoutExpiredMs - System.currentTimeMillis();
+
+                try {
+                    sleep(SLEEP_MS);
+                } catch (InterruptedException e) {
+                    log.error("Exception caused during rule purging...");
+                }
+
+                if (getFlowEntriesByDeviceId(appId, flowRuleService, deviceId).isEmpty()) {
+                    break;
+                } else {
+                    removeFlowRulesByDeviceId(appId, flowRuleService, deviceId);
+                    print("Failed to purging flow rules, retrying rule purging...");
+                }
+
+                if (waitMs <= 0) {
+                    result = false;
+                    break;
+                }
+            }
+            if (result) {
+                print("Successfully purged flow rules for %s!", hostname);
             } else {
-                flowRuleService.removeFlowRulesById(appId);
-                print("Failed to purging flow rules, retrying rule purging...");
-            }
-
-            if (waitMs <= 0) {
-                result = false;
-                break;
+                error("Failed to purge flow rules for %s.", hostname);
             }
         }
+    }
 
-        if (result) {
-            print("Successfully purged flow rules!");
-        } else {
-            error("Failed to purge flow rules.");
-        }
+    private void removeFlowRulesByDeviceId(ApplicationId appId,
+                                           FlowRuleService flowRuleService,
+                                           DeviceId deviceId) {
+        stream(flowRuleService.getFlowEntriesById(appId).spliterator(), false)
+                .filter(flowEntry -> flowEntry.deviceId().equals(deviceId))
+                .forEach(flowRuleService::removeFlowRules);
+    }
+
+    private Set<FlowEntry> getFlowEntriesByDeviceId(ApplicationId appId,
+                                                    FlowRuleService flowRuleService,
+                                                    DeviceId deviceId) {
+        return stream(flowRuleService.getFlowEntriesById(appId).spliterator(), false)
+                .filter(flowEntry -> flowEntry.deviceId().equals(deviceId))
+                .collect(Collectors.toSet());
     }
 }
