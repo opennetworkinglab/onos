811ea2b63db393d2fc8bdccb792023e17d342502
Thomas Vachuska <tom@opennetworking.org>
2020-02-11 10:20:10 -0800

    Add ability to administratively remove ports of an offline device.
    Change-Id: Iaee085be1cd53f783ed80e7c277403eb65ef6d8f

---
+++ cli/src/main/java/org/onosproject/cli/net/DevicePortsRemoveCommand.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2014-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.onosproject.cli.net;
+
+import org.apache.karaf.shell.api.action.Argument;
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Completion;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.onosproject.cli.AbstractShellCommand;
+import org.onosproject.net.DeviceId;
+import org.onosproject.net.device.DeviceAdminService;
+
+/**
+ * Removes an infrastructure device.
+ */
+@Service
+@Command(scope = "onos", name = "device-ports-remove",
+         description = "Removes ports of an infrastructure device")
+public class DevicePortsRemoveCommand extends AbstractShellCommand {
+
+    @Argument(index = 0, name = "uri", description = "Device ID",
+              required = true, multiValued = false)
+    @Completion(DeviceIdCompleter.class)
+    String uri = null;
+
+    @Override
+    protected void doExecute() {
+        try {
+            get(DeviceAdminService.class).removeDevicePorts(DeviceId.deviceId(uri));
+        } catch (IllegalStateException e) {
+            print("There was some issue in removing device ports, please try again");
+        }
+    }
+
+}
