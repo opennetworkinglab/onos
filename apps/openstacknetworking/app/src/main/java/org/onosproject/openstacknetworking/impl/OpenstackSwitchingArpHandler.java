0010580f2db1bc0f22ee9406151e7c7b7007c60e
Jian Li <pyguni@gmail.com>
2020-02-25 03:30:20 +0900

    Fix: remove network related flow rules on openstack network removal
    Change-Id: I3f6501cfd2474665ed244ff10ae604e7aac2f801

--- apps/openstacknetworking/app/src/main/java/org/onosproject/openstacknetworking/impl/OpenstackSwitchingArpHandler.java
+++ apps/openstacknetworking/app/src/main/java/org/onosproject/openstacknetworking/impl/OpenstackSwitchingArpHandler.java
@@ -872,7 +872,7 @@ public class OpenstackSwitchingArpHandler {
             Network network = event.subject();
 
             if (network == null) {
-                log.warn("Network is not specified.");
+                log.debug("Network is not specified.");
                 return false;
             } else {
                 return network.getProviderSegID() != null;
@@ -897,9 +897,10 @@ public class OpenstackSwitchingArpHandler {
                 case OPENSTACK_NETWORK_UPDATED:
                     eventExecutor.execute(() -> processNetworkCreation(event));
                     break;
-                case OPENSTACK_NETWORK_REMOVED:
+                case OPENSTACK_NETWORK_PRE_REMOVED:
                     eventExecutor.execute(() -> processNetworkRemoval(event));
                     break;
+                case OPENSTACK_NETWORK_REMOVED:
                 case OPENSTACK_PORT_CREATED:
                 case OPENSTACK_PORT_UPDATED:
                 case OPENSTACK_PORT_REMOVED:
