fbd65dbd60d20878a3576de1fbd68d4bdee6f26c
Seyeon Jeong <seyeon@opennetworking.org>
2020-03-03 17:33:40 -0800

    Fix an action of t3-troubleshot-mcast command when no sink for a multicast route
    - Prints a message saying that there is no sink to trace
    
    Change-Id: Ib113170c2c2a73e8a71b3819d2b814113adf357d

--- apps/t3/app/src/main/java/org/onosproject/t3/cli/TroubleshootMcastCommand.java
+++ apps/t3/app/src/main/java/org/onosproject/t3/cli/TroubleshootMcastCommand.java
@@ -79,7 +79,7 @@ public class TroubleshootMcastCommand extends AbstractShellCommand {
                         print("Failure: %s", trace.resultMessage());
                         failedTraces.add(trace);
                     } else {
-                        print("Success");
+                        print(trace.resultMessage());
                     }
                 }
             } else {
