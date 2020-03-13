fb3a1bbd13f3dc6172be5a741a6008f6de61bf9f
Esin Karaman <esin.karaman@netsia.com>
2020-03-13 14:38:14 +0000

    VOL-2734 Adding inner vlan id field to McastConfig to configure inner vlan id value for double-tagged multicast flows. The full path of the task: https://jira.opencord.org/browse/VOL-2734
    Change-Id: I45dcd26fd91fba81184e4bf59640f6e43da535cc

--- core/api/src/main/java/org/onosproject/net/config/basics/McastConfig.java
+++ core/api/src/main/java/org/onosproject/net/config/basics/McastConfig.java
@@ -28,11 +28,12 @@ import org.onosproject.net.config.Config;
 public class McastConfig extends Config<ApplicationId> {
     private static final String INGRESS_VLAN = "ingressVlan";
     private static final String EGRESS_VLAN = "egressVlan";
+    private static final String EGRESS_INNER_VLAN = "egressInnerVlan";
 
     @Override
     public boolean isValid() {
-        return hasOnlyFields(INGRESS_VLAN, EGRESS_VLAN) &&
-                ingressVlan() != null && egressVlan() != null;
+        return hasOnlyFields(INGRESS_VLAN, EGRESS_VLAN, EGRESS_INNER_VLAN) &&
+                ingressVlan() != null && egressVlan() != null && egressInnerVlan() != null;
     }
 
     /**
@@ -95,6 +96,38 @@ public class McastConfig extends Config<ApplicationId> {
             object.remove(EGRESS_VLAN);
         } else {
             object.put(EGRESS_VLAN, vlanId.toString());
+        }
+        return this;
+    }
+
+    /**
+     * Gets egress inner VLAN of multicast traffic.
+     *
+     * @return Egress inner VLAN ID
+     */
+    public VlanId egressInnerVlan() {
+        if (!object.has(EGRESS_INNER_VLAN)) {
+            return VlanId.NONE;
+        }
+
+        try {
+            return VlanId.vlanId(object.path(EGRESS_INNER_VLAN).asText());
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    }
+
+    /**
+     * Sets egress inner VLAN of multicast traffic.
+     *
+     * @param vlanId Egress inner VLAN ID
+     * @return this {@link McastConfig}
+     */
+    public McastConfig setEgressInnerVlan(VlanId vlanId) {
+        if (vlanId == null) {
+            object.remove(EGRESS_INNER_VLAN);
+        } else {
+            object.put(EGRESS_INNER_VLAN, vlanId.toString());
         }
         return this;
     }
