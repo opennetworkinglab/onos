388ec251933af2ba5491f46c6becd4156f54269e
pier <pier@opennetworking.org>
2020-04-15 20:53:14 +0200

    Fix for duplicated event issue.
    1) Waits for the end of the leader election;
    
    2) Partially reverts 18417 - there is no always the need
    to notify the delegate;
    
    3) Reverts 18899, since 19904 partially reverted 16278
    which was the original reason for 18899;
    
    4) Removes offline timestamp after an admin-removal
    
    patch2: introduces 4) to fix the issues of the previous patch
    
    patch1: duplicated events are still present after an admin
    removal (remove or wipe-out please) - the issue is not related
    to the changes reported above.
    
    Change-Id: I6e6b2ce21b08d08e8188dea362a904e6dc5505d4

--- core/net/src/main/java/org/onosproject/net/device/impl/DeviceManager.java
+++ core/net/src/main/java/org/onosproject/net/device/impl/DeviceManager.java
@@ -19,6 +19,7 @@ import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
+import com.google.common.util.concurrent.Futures;
 import org.onlab.util.KryoNamespace;
 import org.onlab.util.Tools;
 import org.onosproject.cluster.ClusterService;
@@ -388,7 +389,6 @@ public class DeviceManager
         DeviceEvent event = store.removeDevice(deviceId);
         if (event != null) {
             log.info("Device {} administratively removed", deviceId);
-            post(event);
         }
     }
 
@@ -588,10 +588,12 @@ public class DeviceManager
                 deviceDescription = deviceAnnotationOp.combine(deviceId, deviceDescription, Optional.of(annoConfig));
             }
 
-            MastershipRole role = mastershipService.requestRoleForSync(deviceId);
+            // Wait for the end of the election. sync call of requestRoleFor
+            // wait only 3s and it is not entirely safe since the leadership
+            // election timer can be higher.
+            MastershipRole role = Futures.getUnchecked(mastershipService.requestRoleFor(deviceId));
             log.info("Local role is {} for {}", role, deviceId);
-            DeviceEvent event = store.createOrUpdateDevice(provider().id(), deviceId,
-                    deviceDescription);
+            store.createOrUpdateDevice(provider().id(), deviceId, deviceDescription);
             applyRole(deviceId, role);
 
             if (portConfig != null) {
@@ -609,11 +611,6 @@ public class DeviceManager
                 log.info("Device {} connected", deviceId);
             } else {
                 log.info("Device {} registered", deviceId);
-            }
-
-            if (event != null) {
-                log.trace("event: {} {}", event.type(), event);
-                post(event);
             }
         }
 
@@ -1123,7 +1120,7 @@ public class DeviceManager
                             (dev == null) ? null : BasicDeviceOperator.descriptionOf(dev);
                     desc = BasicDeviceOperator.combine(cfg, desc);
                     if (desc != null && dp != null) {
-                        de = store.createOrUpdateDevice(dp.id(), did, desc);
+                        store.createOrUpdateDevice(dp.id(), did, desc);
                     }
                 }
             } else if (event.configClass().equals(PortDescriptionsConfig.class)) {
@@ -1149,7 +1146,7 @@ public class DeviceManager
                 Optional<Config> prevConfig = event.prevConfig();
                 desc = deviceAnnotationOp.combine(did, desc, prevConfig);
                 if (desc != null && dp != null) {
-                    de = store.createOrUpdateDevice(dp.id(), did, desc);
+                    store.createOrUpdateDevice(dp.id(), did, desc);
                 }
             } else if (portOpsIndex.containsKey(event.configClass())) {
                 ConnectPoint cpt = (ConnectPoint) event.subject();
