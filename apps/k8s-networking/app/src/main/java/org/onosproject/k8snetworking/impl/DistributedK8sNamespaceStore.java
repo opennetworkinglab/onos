adebcbce40e814a83de095a8cada8a25517f6fe9
Jian Li <pyguni@gmail.com>
2020-09-23 01:25:08 +0900

    Fix: add missing FieldsV1 class to kryo registry
    Change-Id: I5dcad880628b8a9a3045fe0565754195510b925d

--- apps/k8s-networking/app/src/main/java/org/onosproject/k8snetworking/impl/DistributedK8sNamespaceStore.java
+++ apps/k8s-networking/app/src/main/java/org/onosproject/k8snetworking/impl/DistributedK8sNamespaceStore.java
@@ -16,6 +16,7 @@
 package org.onosproject.k8snetworking.impl;
 
 import com.google.common.collect.ImmutableSet;
+import io.fabric8.kubernetes.api.model.FieldsV1;
 import io.fabric8.kubernetes.api.model.ManagedFieldsEntry;
 import io.fabric8.kubernetes.api.model.Namespace;
 import io.fabric8.kubernetes.api.model.NamespaceSpec;
@@ -76,6 +77,7 @@ public class DistributedK8sNamespaceStore
             .register(NamespaceSpec.class)
             .register(NamespaceStatus.class)
             .register(ManagedFieldsEntry.class)
+            .register(FieldsV1.class)
             .register(LinkedHashMap.class)
             .build();
 
