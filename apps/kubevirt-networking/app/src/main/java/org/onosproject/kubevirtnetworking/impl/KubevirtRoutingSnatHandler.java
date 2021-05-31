328fb60363e0f05d81439b4c46da818a7bf57556
Daniel Park <dan.park@sk.com>
2021-05-31 13:49:53 +0900

    Enhance the RM logic for Kubevirt Networking App in case of virtual network.
    Change-Id: I9b38ea0d76ce269cb03bc8a0f7d1883ddb92f51c
    (cherry picked from commit d77b4ccb6cd06da7019980add8188b9021f6fcc5)

--- apps/kubevirt-networking/app/src/main/java/org/onosproject/kubevirtnetworking/impl/KubevirtRoutingSnatHandler.java
+++ apps/kubevirt-networking/app/src/main/java/org/onosproject/kubevirtnetworking/impl/KubevirtRoutingSnatHandler.java
@@ -319,19 +319,13 @@ public class KubevirtRoutingSnatHandler {
                 install);
 
         if (network.type() == VXLAN || network.type() == GENEVE || network.type() == GRE) {
-            setDownStreamRulesToGatewayTunBridge(router, network, kubevirtPort, install);
+            setDownStreamRulesToGatewayTunBridge(network, gatewayNode, kubevirtPort, install);
         }
     }
 
-    private void setDownStreamRulesToGatewayTunBridge(KubevirtRouter router,
-                                                      KubevirtNetwork network,
+    private void setDownStreamRulesToGatewayTunBridge(KubevirtNetwork network,
+                                                      KubevirtNode electedGw,
                                                       KubevirtPort port, boolean install) {
-        KubevirtNode electedGw = gatewayNodeForSpecifiedRouter(kubevirtNodeService, router);
-
-        if (electedGw == null) {
-            return;
-        }
-
         KubevirtNode workerNode = kubevirtNodeService.node(port.deviceId());
         if (workerNode == null) {
             return;
