10c45cc52803732f6c426c096007b1d1c7d8f464
Daniele Moro <daniele@opennetworking.org>
2021-06-24 18:17:30 +0200

    [SDFAB-256] Add CLI command for reading content of the UPF store
    Change-Id: If8c363d058b53a7bba60d5dbf76ee1b8b885d7c7
    (cherry picked from commit 9e1e5df971f13c98a61fcdee768d90f6c6367684)

---
+++ pipelines/fabric/impl/src/main/java/org/onosproject/pipelines/fabric/impl/behaviour/cli/ReadInternalUpfStoreCommand.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2021-present Open Networking Foundation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.onosproject.pipelines.fabric.impl.behaviour.cli;
+
+import org.apache.karaf.shell.api.action.Command;
+import org.apache.karaf.shell.api.action.Option;
+import org.apache.karaf.shell.api.action.lifecycle.Service;
+import org.onosproject.cli.AbstractShellCommand;
+import org.onosproject.pipelines.fabric.impl.behaviour.upf.FabricUpfStore;
+import org.onosproject.pipelines.fabric.impl.behaviour.upf.UpfRuleIdentifier;
+
+import java.util.Map;
+
+/**
+ * Read internal UPF store of fabric.
+ */
+@Service
+@Command(scope = "fabric", name = "upf-read-internal-store",
+        description = "Print internal UPF stores")
+public class ReadInternalUpfStoreCommand extends AbstractShellCommand {
+    @Option(name = "-v", aliases = "--verbose",
+            description = "Print more detail of each entry",
+            required = false, multiValued = false)
+    private boolean verbose = false;
+
+    @Override
+    protected void doExecute() {
+        FabricUpfStore upfStore = get(FabricUpfStore.class);
+
+        if (upfStore == null) {
+            print("Error: FabricUpfStore is null");
+            return;
+        }
+
+        Map<UpfRuleIdentifier, Integer> farIdMap = upfStore.getFarIdMap();
+        print("farIdMap size: " + farIdMap.size());
+        if (verbose) {
+            farIdMap.entrySet().forEach(entry -> print(entry.toString()));
+        }
+    }
+}
