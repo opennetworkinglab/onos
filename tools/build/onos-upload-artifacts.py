74ca485314b709d05b0a410f6be19e40693e32ae
Renato Ambrosone <s279186@studenti.polito.it>
2022-11-19 01:19:14 +0100

    bug fix for print in onos-upload-artifacts

--- tools/build/onos-upload-artifacts.py
+++ tools/build/onos-upload-artifacts.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 """
  Copyright 2018-present Open Networking Foundation
 
@@ -147,8 +147,8 @@ def stage_file(file, repo_id, dest):
         with open(file, 'rb') as f:
             r = requests.post(url, data=f.read(), headers=headers, auth=(SONATYPE_USER, SONATYPE_PASSWORD))
             if r.status_code != 201:
-                print((r.status_code))
-                print((r.text))
+                print (r.status_code)
+                print (r.text)
                 sys.exit(1)
     else:
         # deploy to local repo
@@ -165,7 +165,7 @@ def stage_files(files, dest):
 
 
 def upload_file(src, dest):
-    print(("publishing: " + dest.replace("org/onosproject", "")))
+    print ("publishing: " + dest.replace("org/onosproject", ""))
     files = generate_metadata_files(src, dest)
     stage_files(files, dest)
 
@@ -199,12 +199,12 @@ if __name__ == '__main__':
             sys.exit(1)
 
         if SONATYPE_PROFILE is None:
-            print("Environment variable SONATYPE_PROFILE must be set")
+            print ("Environment variable SONATYPE_PROFILE must be set")
             sys.exit(1)
 
-        print(("Uploading to remote repo: " + destination_repo_url))
+        print ("Uploading to remote repo: " + destination_repo_url)
     else:
-        print(("Installing in local repo: " + local_maven_repo))
+        print ("Installing in local repo: " + local_maven_repo)
 
     list_file = open(input_list_file, "r")
     lines = list_file.readlines()
